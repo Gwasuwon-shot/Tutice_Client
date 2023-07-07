5c2fc9bd8d2776eec546c56a2bed6475173eee8a
Seojisoo20191941 <amy0480@naver.com>
2023-07-08 05:46:17 +0900

    [ init ] 글씨체 하단 정렬

--- src/style/globalStyle.ts
+++ src/style/globalStyle.ts
@@ -1,13 +1,6 @@
 import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyle = createGlobalStyle`
-@font-face {
-  font-family: 'Pretendard-Regular';
-  src: url('https://cdn.jsdelivr.net/gh/Project-Noonnu/noonfonts_2107@1.1/Pretendard-Regular.woff') format('woff');
-  font-weight: 400;
-  font-style: normal;
-}
-
   :root {
     --vh: 100%;
    }
@@ -99,4 +92,11 @@ export const GlobalStyle = createGlobalStyle`
     }
   }
  
+  @font-face {
+  font-family: 'Pretendard-Regular';
+  src: url('https://cdn.jsdelivr.net/gh/Project-Noonnu/noonfonts_2107@1.1/Pretendard-Regular.woff') format('woff');
+  font-weight: 400;
+  font-style: normal;
+}
+
 `;

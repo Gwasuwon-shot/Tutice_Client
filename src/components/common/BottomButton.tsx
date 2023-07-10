fa1645fbb9d0672c3903190960e4728253058dac
Eunbeann <ds5bean23@gmail.com>
2023-07-11 01:45:22 +0900

    [ style ] Role 마무리

---
+++ src/components/common/BottomButton.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { styled } from "styled-components";
+
+export default function BottomButton({ children }) {
+  return (
+    <BottomContainer>
+      <BottomText> {children} </BottomText>
+    </BottomContainer>
+  );
+}
+
+const BottomContainer = styled.button`
+  position: fixed;
+  bottom: 0;
+
+  width: 32rem;
+  height: 6.3rem;
+  margin-left: -1.6rem;
+
+  background-color: ${({ theme }) => theme.colors.green5};
+`;
+
+const BottomText = styled.div`
+  position: relative;
+
+  /* top- 정확한 값으로 수정 필요 */
+  top: -1rem;
+
+  color: ${({ theme }) => theme.colors.grey0};
+  ${({ theme }) => theme.fonts.body01};
+`;

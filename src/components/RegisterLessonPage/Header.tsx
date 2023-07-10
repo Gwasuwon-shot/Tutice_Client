3f67409f15164c6671c0aab8acfbc186498fa3d4
parkheeddong <phj7214go@gmail.com>
2023-07-10 22:00:59 +0900

    [ fix ] 폰트 적용 오류 해결

--- src/components/RegisterLessonPage/Header.tsx
+++ src/components/RegisterLessonPage/Header.tsx
@@ -4,35 +4,44 @@ import styled from 'styled-components';
 export default function Header() {
 
     return (
-    <>  
+    
         <HeaderWrapper>
             <RegisterLessonHeaderIc />
-            <InputHeader> 학생의 이름과 <br/> 과목을 입력해주세요. <Emphasis> * </Emphasis></InputHeader> 
+            <InputHeader> 학생의 이름과 <br/> 과목을 입력해주세요. <Emphasis> * </Emphasis> </InputHeader> 
             <InputNotice> 수업관리를 도와드릴 수 있도록 몇가지 정보를 알려주세요.</InputNotice>    
         </HeaderWrapper>
-    </>
+
     );
 }
 
 const HeaderWrapper = styled.header`
     display: flex;
     flex-direction: column;
+    height: 15.8rem;
 `
 
-const InputHeader = styled.h1`
+const InputHeader = styled.div`
     display: flex;
-
-    ${({ theme }) => theme.fonts.title1};
+    display:
+    margin-top: 0.9rem;
+    margin-left: 1.6rem;
+    ${({ theme }) => theme.fonts.title01};
     color: ${({ theme }) => theme.colors.grey900};
 `
 
 const Emphasis = styled.span`
-    ${({ theme }) => theme.fonts.title1};
-    color: ${({ theme }) => theme.colors.green05};
+    display: flex;
+    align-items: flex-end;
+    ${({ theme }) => theme.fonts.title01};
+    color: ${({ theme }) => theme.colors.green5};
 `
 
 const InputNotice = styled.h2`
     display: flex;
+    
+    margin-top: 1.1rem;
+    margin-left: 1.6rem;
+    
     ${({ theme }) => theme.fonts.body05};
     color: ${({ theme }) => theme.colors.grey600};
 `
--- src/components/RegisterLessonPage/LessonInput.tsx
+++ src/components/RegisterLessonPage/LessonInput.tsx
@@ -6,24 +6,56 @@ export default function LessonInput() {
     let lessonName;
     
     return (
-    <>
-        <InputName> 학생이름 </InputName>
-        <StudentInput type = 'text' placeholder = '이름을 입력하세요'/>
+        
+    <InputWrapper>
 
-        <InputName> 과목 </InputName>
-        <SubjectInput type = 'text' placeholder = '수업과목을 입력하세요' />
-    </>
+        <InputSection>
+            <InputName> 학생이름 </InputName> 
+            <StudentInput type = 'text' placeholder = '이름을 입력하세요'/>
+        </InputSection>
+
+        <InputSection>
+            <InputName> 과목 </InputName>
+            <SubjectInput type = 'text' placeholder = '수업과목을 입력하세요' />
+        </InputSection>
+
+    
+    </InputWrapper>
     );
 }
 
-const InputName = styled.h1`
+const InputWrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+`
 
+const InputSection = styled.section`
+    display: flex;
+    flex-direction: column;
+    width: 29.2rem;
+    height: 5.6rem;
+`
+
+const InputName = styled.h1` 
+    display: flex;
+    margin-bottom: 1rem;
+        
+    ${({ theme }) => theme.fonts.body04};
+    color: ${({ theme }) => theme.colors.grey300};
 `
 
 const StudentInput = styled.input`
-
+    display: flex;
+    margin-bottom: 1rem;
+    ${({ theme }) => theme.fonts.title03};
+    color: ${({ theme }) => theme.colors.grey400};
 `
 
 const SubjectInput = styled.input`
-
+    display: flex;
+    margin-bottom: 1rem;
+    ${({ theme }) => theme.fonts.title03};
+    color: ${({ theme }) => theme.colors.grey400};
 `

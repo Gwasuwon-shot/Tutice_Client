c3926d36333c459b03ba7a0d7759d318f7ab7763
Seojisoo20191941 <amy0480@naver.com>
2023-07-10 17:35:44 +0900

    [ feat ] 푸터 기본 뷰 구현

---
+++ src/components/common/TeacherFooter.tsx
@@ -0,0 +1,28 @@
+import styled from "styled-components";
+
+export default function TeacherFooter() {
+  return <TeacherFooterWrapper>TeacherFooter</TeacherFooterWrapper>;
+}
+
+const TeacherFooterWrapper = styled.footer`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: absolute;
+  bottom: 0;
+  z-index: 10;
+
+  width: 32rem;
+  height: 7.2rem;
+  padding: 0.3rem 1.5rem 1.9rem;
+
+  background: ${({ theme }) => theme.colors.grey0};
+
+  flex-shrink: 0;
+
+  border-radius: 18px 18px 0 0;
+
+  border-top: 1px solid ${({ theme }) => theme.colors.grey50};
+
+  box-shadow: 0 0 0.5rem 0 rgb(56 62 68 / 8%);
+`;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,11 @@
 import Header from "../components/common/Header";
+import TeacherFooter from "../components/common/TeacherFooter";
 
 export default function Home() {
   return (
     <>
       <Header />
+      <TeacherFooter />
     </>
   );
 }

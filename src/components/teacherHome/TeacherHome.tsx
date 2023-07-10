6f47caa855acc82e106735989b1a0d5b1beb453a
Seojisoo20191941 <amy0480@naver.com>
2023-07-10 23:22:13 +0900

    [ design ] 선생님 홈 뷰 마진 조정

--- src/components/teacherHome/TeacherHome.tsx
+++ src/components/teacherHome/TeacherHome.tsx
@@ -1,13 +1,18 @@
+import { styled } from "styled-components";
 import Header from "../common/Header";
 import TeacherFooter from "../common/TeacherFooter";
 import Banner from "./Banner";
 
 export default function TeacherHome() {
   return (
-    <>
+    <TeacherHomeWrapper>
       <Header />
       <Banner />
       <TeacherFooter />
-    </>
+    </TeacherHomeWrapper>
   );
 }
+
+const TeacherHomeWrapper = styled.div`
+  margin: 0 1.4rem;
+`;
--- src/components/teacherHome/WelcomeTeacher.tsx
+++ src/components/teacherHome/WelcomeTeacher.tsx
@@ -8,13 +8,17 @@ export default function WelcomeTeacher(props: WelcomeTeacherProp) {
   const { teacherName } = props;
 
   return (
-    <>
+    <WelcomeTeacherWrapper>
       <WelcomeText>{teacherName} 선생님</WelcomeText>
       <WelcomeText>수고하셨어요!</WelcomeText>
-    </>
+    </WelcomeTeacherWrapper>
   );
 }
 
 const WelcomeText = styled.h1`
-  ${({ theme }) => theme.fonts.title01}
+  ${({ theme }) => theme.fonts.title01};
+`;
+
+const WelcomeTeacherWrapper = styled.header`
+  margin: 2rem 0 1.2rem 0.4rem;
 `;

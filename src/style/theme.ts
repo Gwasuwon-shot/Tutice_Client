d8babe333dc4adec625830dd6418b4c77f941263
Seojisoo20191941 <amy0480@naver.com>
2023-07-11 00:00:25 +0900

    [ design ] 색깔 변경

--- src/style/theme.ts
+++ src/style/theme.ts
@@ -6,7 +6,7 @@ const colors = {
   green2: "#B0E0D6",
   green3: "#7DCDBD",
   green4: "#45B9A2",
-  green5: "#0DA98F",
+  green5: "#0DA98E",
   green6: "#00997D",
   green7: "#038F7E",
   green8: "#007C61",

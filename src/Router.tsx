4cbd6e05dcfda0e2cc2e788547bcd5ed8cd56379
parkheeddong <phj7214go@gmail.com>
2023-07-10 18:15:27 +0900

    [ init ] 초기 세팅

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+
 import CheckAttendance from "./pages/CheckAttendance";
 import CheckLesson from "./pages/CheckLesson";
 import Login from "./pages/Login";

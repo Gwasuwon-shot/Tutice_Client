1fdefaecd49087f69bb4e300bfc27c65d27d5cfb
pinktopaz <heather128@ewhain.net>
2023-07-10 23:03:00 +0900

    [ assets ] 두 번째 스와이퍼 로고 추가

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,1 +1,4 @@
 export { ReactComponent as MainIc } from "./icon/mainIc.svg";
+export { ReactComponent as MainLogo } from "./icon/MainLogoOnBoardingIcon.svg";
+export { ReactComponent as TuticeTitleLogo } from "./icon/TuticeTitleLogoOnBoardingIcon.svg";
+export { ReactComponent as MainLogoGradation } from "./icon/MainLogoGradationOnBoardingIcon.svg";

8c40129f825f52471a6751b1a32c6fae34579326
parkheeddong <phj7214go@gmail.com>
2023-07-10 21:00:59 +0900

    [ assets ] icon 추가

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,1 +1,2 @@
 export { ReactComponent as MainIc } from "./icon/mainIc.svg";
+export { ReactComponent as RegisterLessonHeaderIc } from './icon/registerLessonHeaderIc.svg'
--- src/components/RegisterLessonPage/Header.tsx
+++ src/components/RegisterLessonPage/Header.tsx
@@ -1,13 +1,15 @@
+import { RegisterLessonHeaderIc } from '../../assets';
 import styled from 'styled-components';
 
 export default function Header() {
 
     return (
     <>  
-    <HeaderWrapper>
-        <InputHeader> 학생의 이름과 <br/> 과목을 입력해주세요.</InputHeader>    
-        <InputNotice> 수업관리를 도와드릴 수 있도록 몇가지 정보를 알려주세요.</InputNotice>    
-    </HeaderWrapper>
+        <HeaderWrapper>
+            <RegisterLessonHeaderIc />
+            <InputHeader> 학생의 이름과 <br/> 과목을 입력해주세요. <Emphasis> * </Emphasis></InputHeader> 
+            <InputNotice> 수업관리를 도와드릴 수 있도록 몇가지 정보를 알려주세요.</InputNotice>    
+        </HeaderWrapper>
     </>
     );
 }
@@ -15,8 +17,6 @@ export default function Header() {
 const HeaderWrapper = styled.header`
     display: flex;
     flex-direction: column;
-
-    
 `
 
 const InputHeader = styled.h1`
@@ -26,10 +26,14 @@ const InputHeader = styled.h1`
     color: ${({ theme }) => theme.colors.grey900};
 `
 
+const Emphasis = styled.span`
+    ${({ theme }) => theme.fonts.title1};
+    color: ${({ theme }) => theme.colors.green05};
+`
+
 const InputNotice = styled.h2`
     display: flex;
-    
-    ${({ theme }) => theme.fonts.body5};
+    ${({ theme }) => theme.fonts.body05};
     color: ${({ theme }) => theme.colors.grey600};
 `
 

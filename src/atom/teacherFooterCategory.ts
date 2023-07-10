5d7586923e38644c89cdc25a4374618fbfb53d39
Seojisoo20191941 <amy0480@naver.com>
2023-07-10 18:37:23 +0900

    [ feat ] 푸터 아이콘 클릭 시 활성화 아이콘으로 변경되도록 구현

---
+++ src/atom/teacherFooterCategory.ts
@@ -0,0 +1,10 @@
+// import { atom } from "recoil";
+// import { recoilPersist } from "recoil-persist";
+
+// const { persistAtom } = recoilPersist();
+
+// export const teacherFooterCategory = atom<string>({
+//   key: "teacherFooterCategory",
+//   default: "",
+//   effects_UNSTABLE: [persistAtom],
+// });
--- src/components/common/TeacherFooter.tsx
+++ src/components/common/TeacherFooter.tsx
@@ -1,15 +1,48 @@
 import { useState } from "react";
 import styled from "styled-components";
-import { TEACHER_FOOTER } from "../../core/teacherFooter";
+import {
+  CalendarActiveTeacherFooterIc,
+  CalendarTeacherFooterIc,
+  ClassManagingActiveTeacherFooterIc,
+  ClassManagingTeacherFooterIc,
+  HomeActiveTeacherFooterIc,
+  HomeTeacherFooterIc,
+  MyActiveTeacherFooterIc,
+  MyTeacherFooterIc,
+} from "../../assets";
+import { TEACHER_FOOTER, TEACHER_FOOTER_CATEGORY } from "../../core/teacherFooter";
 import { TeacherFooterType } from "../../type/teacherFooterType";
 
 export default function TeacherFooter() {
   const [teacherFooterList, setTeacherFooterList] = useState<TeacherFooterType[]>(TEACHER_FOOTER);
 
+  function showTeacherFooterIcon(category: string, isMoved: boolean) {
+    switch (category) {
+      case TEACHER_FOOTER_CATEGORY.home:
+        return isMoved ? <HomeActiveTeacherFooterIc /> : <HomeTeacherFooterIc />;
+      case TEACHER_FOOTER_CATEGORY.calendar:
+        return isMoved ? <CalendarActiveTeacherFooterIc /> : <CalendarTeacherFooterIc />;
+      case TEACHER_FOOTER_CATEGORY.classManaging:
+        return isMoved ? <ClassManagingActiveTeacherFooterIc /> : <ClassManagingTeacherFooterIc />;
+      case TEACHER_FOOTER_CATEGORY.my:
+        return isMoved ? <MyActiveTeacherFooterIc /> : <MyTeacherFooterIc />;
+      default:
+        return;
+    }
+  }
+
+  function handleMoveToPage(id: number) {
+    setTeacherFooterList(
+      teacherFooterList.map((list) => (list.id === id ? { ...list, isMoved: true } : { ...list, isMoved: false })),
+    );
+  }
+
   return (
     <TeacherFooterWrapper>
       {teacherFooterList.map(({ id, category, isMoved }) => (
-        <i key={id}>{}</i>
+        <i key={id} onClick={() => handleMoveToPage(id)}>
+          {showTeacherFooterIcon(category, isMoved)}
+        </i>
       ))}
     </TeacherFooterWrapper>
   );

351fb96b53c14d53476a60d0db64a081e082a216
Seojisoo20191941 <amy0480@naver.com>
2023-07-10 21:23:48 +0900

    [ feat ] 선생님 홈 더미 데이터 삽입

---
+++ src/components/teacherHome/TeacherHome.tsx
@@ -0,0 +1,12 @@
+import Header from "../common/Header";
+import TeacherFooter from "../common/TeacherFooter";
+
+export default function TeacherHome() {
+  return (
+    <>
+      <Header />
+      <TeacherFooter />
+    </>
+  );
+}
+0;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,11 @@
-import Header from "../components/common/Header";
-import TeacherFooter from "../components/common/TeacherFooter";
+import TeacherHome from "../components/teacherHome/TeacherHome";
 
 export default function Home() {
   return (
     <>
-      <Header />
-      <TeacherFooter />
+      {/* 선생님인 경우 */}
+      <TeacherHome />
+      {/* 학부모인 경우 ParentHome 컴포넌트 띄워주기 */}
     </>
   );
 }

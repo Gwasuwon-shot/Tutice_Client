da4908073078a4f20158a1c57bc409aa9f60db3f
Seojisoo20191941 <amy0480@naver.com>
2023-07-11 23:15:08 +0900

    [ design ] 커서 포인터 추가

--- src/components/teacherHome/AlarmBanner.tsx
+++ src/components/teacherHome/AlarmBanner.tsx
@@ -20,9 +20,11 @@ export default function AlarmBanner(props: AlarmBannerProps) {
 const MissingMaintenanceTeacherHomeIcon = styled(MissingMaintenanceTeacherHomeIc)`
   width: 29.2rem;
   margin-top: 1.6rem;
+  cursor: pointer;
 `;
 
 const MissingAttendaceTeacherHomeIcon = styled(MissingAttendaceTeacherHomeIc)`
   width: 29.2rem;
   margin-top: 1rem;
+  cursor: pointer;
 `;

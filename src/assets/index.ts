2c7bd0921e1dc15870003d2f4021f335064be751
HAE2NI <hae0_02ni@ewhain.net>
2023-07-12 01:43:08 +0900

    [ assets ] 버튼 asset 추가

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,1 +1,3 @@
 export { ReactComponent as MainIc } from "./icon/mainIc.svg";
+export { ReactComponent as NextMonthArrowButton } from "./icon/arrow_cal_right.svg";
+export { ReactComponent as PrevMonthArrowButton } from "./icon/arrow_cal_left.svg";

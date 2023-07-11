d0465f5f730aa4d1dcf2dea7c5a11622305aa720
Seojisoo20191941 <amy0480@naver.com>
2023-07-11 13:53:48 +0900

    [ design ] 배너 스타일 조정

--- src/components/teacherHome/banner/ClassPreviewBanner.tsx
+++ src/components/teacherHome/banner/ClassPreviewBanner.tsx
@@ -1,3 +1,5 @@
+import { styled } from "styled-components";
+import { CLASS_PREVIEW_BANNER_COMMENTS } from "../../../core/teacherHome/classPreviewBannerComments";
 import { TeacherHomeTodayScheduleType } from "../../../type/teacherHomeTodayScheduleType";
 import SubjectLabel from "../../common/SubjectLabel";
 
@@ -13,17 +15,63 @@ export default function ClassPreviewBanner(props: ClassPreviewBannerProps) {
   function showClassPreviewComment(timeStatus: number) {
     switch (timeStatus) {
       case 1:
-        break;
+        return CLASS_PREVIEW_BANNER_COMMENTS.upcoming;
       case 2:
-        break;
+        return CLASS_PREVIEW_BANNER_COMMENTS.ing;
+      case 3:
+        return CLASS_PREVIEW_BANNER_COMMENTS.end;
+      default:
+        return;
     }
   }
 
   return (
-    <>
-      {studentName} 학생 <SubjectLabel subject={subject} backgroundColor="#B0E0D6" color="#00997D" />
-      {count} 회차 수업이
-      {showClassPreviewComment(timeStatus)}
-    </>
+    <ClassPreviewBannerWrapper>
+      <StudentNameWrapper>
+        <b>{studentName}</b> 학생
+        <SubjectLabel subject={subject} backgroundColor="#B0E0D6" color="#00997D" />
+      </StudentNameWrapper>
+      <ClassStatusWrapper>
+        <ClassCountMentWrapper>{count} 회차 수업이</ClassCountMentWrapper>
+        {showClassPreviewComment(timeStatus)}
+      </ClassStatusWrapper>
+    </ClassPreviewBannerWrapper>
   );
 }
+
+const ClassPreviewBannerWrapper = styled.section`
+  display: flex;
+  justify-content: center;
+  align-items: flex-start;
+  flex-direction: column;
+
+  width: 29.2rem;
+  height: 8rem;
+  padding: 1.6rem 1rem 1.6rem 1.4rem;
+
+  background-color: ${({ theme }) => theme.colors.green5};
+  color: ${({ theme }) => theme.colors.grey0};
+
+  border-radius: 8px;
+`;
+
+const StudentNameWrapper = styled.h1`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  width: 10.8rem;
+
+  ${({ theme }) => theme.fonts.title02};
+`;
+
+const ClassStatusWrapper = styled.p`
+  display: flex;
+
+  margin-top: 0.3rem;
+  ${({ theme }) => theme.fonts.body02};
+`;
+
+const ClassCountMentWrapper = styled.p`
+  margin-right: 0.5rem;
+`;
--- src/core/teacherHome/classPreviewBannerComments.ts
+++ src/core/teacherHome/classPreviewBannerComments.ts
@@ -1,4 +1,4 @@
-export const UPCOMIMG_CLASS_BANNER_COMMENTS = {
+export const CLASS_PREVIEW_BANNER_COMMENTS = {
   upcoming: "예정되어 있어요",
   ing: "진행 중이에요",
   end: "종료되었어요",

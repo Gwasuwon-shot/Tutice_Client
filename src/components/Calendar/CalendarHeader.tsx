9b41a47f23a787ae17a4dd27620d9237cd05e1ae
HAE2NI <hae0_02ni@ewhain.net>
2023-07-11 17:20:52 +0900

    [ feat ] 캘린더 헤더 구현 (년, 월)

---
+++ src/components/Calendar/CalendarHeader.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import styled from "styled-components";
+import { format } from "date-fns";
+
+export default function CalendarHeader({ currentMonth, prevMonth, nextMonth }) {
+  return (
+    <HeaderWrapper>
+      <PrevMonthButton onClick={prevMonth}>{"<"}</PrevMonthButton>
+      <YearMonthWrapper>
+        {format(currentMonth, "yyyy")}년 {format(currentMonth, "MM")}월
+      </YearMonthWrapper>
+      <NextMonthButton onClick={nextMonth}> {">"}</NextMonthButton>
+    </HeaderWrapper>
+  );
+}
+const HeaderWrapper = styled.header`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  font-size: 1.1rem;
+  gap: 1.6rem;
+`;
+
+const YearMonthWrapper = styled.span`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  font-size: 1.1rem;
+`;
+
+const PrevMonthButton = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const NextMonthButton = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;

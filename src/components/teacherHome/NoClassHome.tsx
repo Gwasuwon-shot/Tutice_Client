c2d20ec97c1066c0589dba6d06dc15ffd2d1ac60
Seojisoo20191941 <amy0480@naver.com>
2023-07-12 03:42:05 +0900

    [ design ] 아예 존재하는 수업이 없을 때의 선생님 홈 구현

--- src/components/teacherHome/NoClassHome.tsx
+++ src/components/teacherHome/NoClassHome.tsx
@@ -12,10 +12,12 @@ export default function NoClassHome() {
       <NoClassHomeWrapper>
         <NoClassLogoTeacherHomeIcon />
         <NoClassNotice> 아직 등록된 수업이 없어요!</NoClassNotice>
-        <p> 나무 코드 생성을 통해 학생을 추가하고 </p>
-        <p>링크를 학부모님에게 공유해보세요</p>
+        <SubContext>
+          <p> 나무 코드 생성을 통해 학생을 추가하고 </p>
+          <p>링크를 학부모님에게 공유해보세요</p>
+        </SubContext>
         {/* 성경이가 만들 공통 컴포넌트 연결하기 */}
-        <button>나무코드 생성하기</button>
+        <Button type="button">나무코드 생성하기</Button>
       </NoClassHomeWrapper>
     </>
   );
@@ -32,6 +34,34 @@ const NoClassLogoTeacherHomeIcon = styled(NoClassLogoTeacherHomeIc)`
 `;
 
 const NoClassNotice = styled.h1`
+  margin-top: 1.5rem;
+
   color: ${({ theme }) => theme.colors.green5};
   ${({ theme }) => theme.fonts.title02};
+`;
+
+const SubContext = styled.div`
+  margin: 0.8rem 0 6.2rem;
+
+  color: ${({ theme }) => theme.colors.grey700};
+
+  text-align: center;
+  ${({ theme }) => theme.fonts.body07};
+`;
+
+const Button = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  width: 29.2rem;
+  height: 5rem;
+  padding: 0.8rem;
+
+  background-color: ${({ theme }) => theme.colors.green5};
+  color: ${({ theme }) => theme.colors.white};
+  gap: 0.8rem;
+  flex-shrink: 0;
+
+  border-radius: 8px;
 `;

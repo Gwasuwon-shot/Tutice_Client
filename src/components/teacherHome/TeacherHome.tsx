a094a614ee6e074ba042da03afa4a11f928c0e48
Seojisoo20191941 <amy0480@naver.com>
2023-07-13 02:48:58 +0900

    [ design ] 모달 위치 조정

--- src/components/teacherHome/TeacherHome.tsx
+++ src/components/teacherHome/TeacherHome.tsx
@@ -1,8 +1,5 @@
 import { useState } from "react";
-import { useRecoilState } from "recoil";
 import { styled } from "styled-components";
-import { isModalOpen } from "../../atom/common/isModalOpen";
-import AttendanceCheckModal from "../common/AttendanceCheckModal";
 import Header from "../common/Header";
 import TeacherFooter from "../common/TeacherFooter";
 import NoClassHome from "./NoClassHome";
@@ -11,11 +8,9 @@ import YesClassHome from "./YesClassHome";
 export default function TeacherHome() {
   // 수업이 존재는 하는지에 대한 데이터 패칭
   const [isClassExist, setIsClassExist] = useState(true);
-  const [openModal, setOpenModal] = useRecoilState<boolean>(isModalOpen);
 
   return (
     <>
-      <AttendanceCheckModal />
       <TeacherHomeWrapper>
         <Header />
         {isClassExist ? <YesClassHome /> : <NoClassHome />}

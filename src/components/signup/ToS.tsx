cc939bfd52e9bddc2b7b1aeadcb603322d3adfbc
Eunbeann <ds5bean23@gmail.com>
2023-07-12 17:58:21 +0900

    [ feat ] 체크 박스 임시 배경색

--- src/components/signup/ToS.tsx
+++ src/components/signup/ToS.tsx
@@ -6,28 +6,29 @@ export default function ToS() {
   return (
     <TosWrapper>
       <CheckWrapper>
-        <CheckBox type="checkbox" $TosNoneSignupIc={TosNoneSignupIc} />
+        {/* <CheckBox type="checkbox" $TosNoneSignupIc={TosNoneSignupIc} /> */}
+        <CheckBox type="checkbox" />
         <CheckText> 약관 전체 동의 </CheckText>
         <CheckSubText> 선택항목에 대한 동의 포함 </CheckSubText>
       </CheckWrapper>
       <Horizon />
       <CheckWrapper>
-        <CheckBox type="checkbox" $TosNoneSignupIc={TosNoneSignupIc} />
+        <CheckBox type="checkbox" />
         <Essential>(필수) </Essential>
         <CheckText> 만 14세 이상입니다 </CheckText>
       </CheckWrapper>
       <CheckWrapper>
-        <CheckBox type="checkbox" $TosNoneSignupIc={TosNoneSignupIc} />
+        <CheckBox type="checkbox" />
         <Essential>(필수) </Essential>
         <CheckText> 서비스 이용 약관 동의 </CheckText>
       </CheckWrapper>
       <CheckWrapper>
-        <CheckBox type="checkbox" $TosNoneSignupIc={TosNoneSignupIc} />
+        <CheckBox type="checkbox" />
         <Essential>(필수) </Essential>
         <CheckText> 개인정보 수집 및 이용 동의 </CheckText>
       </CheckWrapper>
       <CheckWrapper>
-        <CheckBox type="checkbox" $TosNoneSignupIc={TosNoneSignupIc} />
+        <CheckBox type="checkbox" />
         <Optional>(선택) </Optional>
         <CheckText> 개인 정보 마케팅 활용 동의 </CheckText>
       </CheckWrapper>
@@ -52,12 +53,23 @@ const CheckWrapper = styled.div`
   margin-bottom: 1.6rem;
 `;
 
-const CheckBox = styled.input<{ $TosNoneSignupIc: string }>`
+// const CheckBox = styled.input<{ $TosNoneSignupIc: string }>`
+//   width: 2rem;
+//   height: 2rem;
+
+//   background-color: red;
+//   background-image: url(${($TosNoneSignupIc) => $TosNoneSignupIc});
+// `;
+const CheckBox = styled.input`
   width: 2rem;
   height: 2rem;
 
   background-color: red;
-  background-image: url(${($TosNoneSignupIc) => $TosNoneSignupIc});
+  background-image: url("$TosNoneSignupIc}");
+
+  &:checked {
+    background-color: blue;
+  }
 `;
 
 const CheckText = styled.p`
@@ -92,4 +104,8 @@ const Optional = styled.p`
 
   ${({ theme }) => theme.fonts.body04};
   margin-right: 0.1rem;
+`;
+
+const HyperLink = styled.link`
+  color: ${({ theme }) => theme.colors.grey500};
 `;

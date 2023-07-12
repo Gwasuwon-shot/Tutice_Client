1b8842d8ffb4c7260b52d5a85c781a4701a43851
Seojisoo20191941 <amy0480@naver.com>
2023-07-12 23:25:14 +0900

    [ fix ] 아이콘 인덱스 겹치는 문제 해결

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,3 +1,5 @@
+export { ReactComponent as PrevMonthArrowButton } from "./icon/arrow_cal_left.svg";
+export { ReactComponent as NextMonthArrowButton } from "./icon/arrow_cal_right.svg";
 export { ReactComponent as AttenceCheckCommonIc } from "./icon/attenceCheckCommonIc.svg";
 export { ReactComponent as CalendarActiveTeacherFooterIc } from "./icon/calendarActiveTeacherFooterIc.svg";
 export { ReactComponent as CalendarTeacherFooterIc } from "./icon/calendarTeacherFooterIc.svg";
@@ -6,10 +8,6 @@ export { ReactComponent as ClassManagingTeacherFooterIc } from "./icon/classMana
 export { ReactComponent as HomeActiveTeacherFooterIc } from "./icon/homeActiveTeacherFooterIc.svg";
 export { ReactComponent as HomeTeacherFooterIc } from "./icon/homeTeacherFooterIc.svg";
 export { ReactComponent as MainIc } from "./icon/mainIc.svg";
-export { ReactComponent as NextMonthArrowButton } from "./icon/arrow_cal_right.svg";
-export { ReactComponent as PrevMonthArrowButton } from "./icon/arrow_cal_left.svg";
-export { ReactComponent as CancelButton } from "./icon/x_btn.svg";
-export { ReactComponent as RegisterLessonHeaderIc } from './icon/registerLessonHeaderIc.svg'
 export { ReactComponent as MissingAttendaceTeacherHomeIc } from "./icon/missingAttendaceTeacherHomeIc.svg";
 export { ReactComponent as MissingMaintenanceTeacherHomeIc } from "./icon/missingMaintenanceTeacherHomeIc.svg";
 export { ReactComponent as MyActiveTeacherFooterIc } from "./icon/myActiveTeacherFooterIc.svg";
@@ -19,3 +17,4 @@ export { ReactComponent as RegisterLessonHeaderIc } from "./icon/registerLessonH
 export { ReactComponent as RightArrowTeacherHomeIc } from "./icon/rightArrowTeacherHomeIc.svg";
 export { ReactComponent as TuticeWithTextCommonIc } from "./icon/tuticeWithTextCommonIc.svg";
 export { ReactComponent as UpcomingClassLogoTeacherHomeIc } from "./icon/upcomingClassLogoTeacherHomeIc.svg";
+export { ReactComponent as CancelButton } from "./icon/x_btn.svg";

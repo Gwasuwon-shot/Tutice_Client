2d811ecce412a08b8af086c3b2aa4359efc85f04
parkheeddong <phj7214go@gmail.com>
2023-07-12 19:23:15 +0900

    [ etc ] 불필요 코드 삭제

--- src/components/common/ProgressBar.tsx
+++ src/components/common/ProgressBar.tsx
@@ -1,38 +1,31 @@
 import React from "react";
 import {styled} from 'styled-components';
 
-interface ProgressBarProps {
+interface ProgressBarProps {    
     progress : number;
 }
 
 export default function ProgressBar({ progress }: ProgressBarProps) {    
-    const getColor = (progress:number) : string => {
-        if (progress === 25) return 'green';
-        if (progress === 50) return 'green';
-        if (progress === 75) return 'green';
-        if (progress === 100) return 'green';
-        return 'transparent';
-    }
-
-    const getWidth = (progress: number): string => {
-        return `${progress}%`;
-      };
 
     return (
         <ProgressBarWrapper>
-            <Progress width = {getWidth(progress)} color = {getColor(progress)} />
+            <Progress width = {`${progress}%`}/>
         </ProgressBarWrapper>
     )
 }
 
-const ProgressBarWrapper = styled.section`
+const ProgressBarWrapper = styled.div`
     width: 100%;
     height: 0.4rem;
-    background-color: lightgray;
+    
+    background-color: ${({ theme }) => theme.colors.grey20};
 `
 
-const Progress = styled.div<{width: string; color: string}>`
-    height: 100%;
+const Progress = styled.div<{width: string;}>`
     width: ${(props) => props.width};
-    background-color: ${(props)=>props.color};
+    height: 100%;
+    
+    background-color: ${({ theme }) => theme.colors.green5};
+    border-top-right-radius : 50px;
+    border-bottom-right-radius: 50px;
 `

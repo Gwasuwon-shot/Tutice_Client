55772194a4fa7ec5cb2446b2059ad83401685f26
Eunbeann <ds5bean23@gmail.com>
2023-07-12 11:20:46 +0900

    [ feat ] assets index 추가

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,1 +1,4 @@
 export { ReactComponent as MainIc } from "./icon/mainIc.svg";
+export { ReactComponent as backButtonSignupIc } from "./icon//backButtonSignupIc.svg";
+export { ReactComponent as RoleCheckSignupIc } from "./icon//RoleCheckSignupIc.svg";
+export { ReactComponent as RoleNoneCheckSignupIc } from "./icon//RoleNoneCheckSignupIc.svg";

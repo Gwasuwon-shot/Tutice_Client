c5698f98211d40c5941c5665f9db2a9c71add9ed
HAE2NI <hae0_02ni@ewhain.net>
2023-07-12 16:21:11 +0900

    [ etc ] 학부모 캘린더 데이터 추가

---
+++ src/core/Parents/ParentsCalendar.ts
@@ -0,0 +1,56 @@
+export const PARENTS_CALENDAR = {
+  code: 200,
+  message: "스케줄 메인 뷰 조회 성공",
+  data: {
+    lessonList: [
+      {
+        lesson: {
+          idx: 1,
+          studentName: "박송현",
+          subject: "수학",
+        },
+        scheduleList: [
+          {
+            idx: 1,
+            date: "2023-07-04",
+            startTime: "17:00",
+            endTime: "19:00",
+          },
+          {
+            idx: 2,
+            date: "2023-07-07",
+            startTime: "17:00",
+            endTime: "19:00",
+          },
+          {
+            idx: 3,
+            date: "2023-07-11",
+            startTime: "17:00",
+            endTime: "19:00",
+          },
+        ],
+      },
+      {
+        lesson: {
+          idx: 2,
+          studentName: "이유나",
+          subject: "영어",
+        },
+        scheduleList: [
+          {
+            idx: 4,
+            date: "2023-07-04",
+            startTime: "19:00",
+            endTime: "21:00",
+          },
+          {
+            idx: 5,
+            date: "2023-07-07",
+            startTime: "19:00",
+            endTime: "21:00",
+          },
+        ],
+      },
+    ],
+  },
+};

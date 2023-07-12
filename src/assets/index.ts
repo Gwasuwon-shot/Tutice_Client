4d843b6640768d8c747ff1e2641bb1b3faab8855
HAE2NI <hae0_02ni@ewhain.net>
2023-07-12 15:06:33 +0900

    [ assets ] x버튼 추가

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,3 +1,4 @@
 export { ReactComponent as MainIc } from "./icon/mainIc.svg";
 export { ReactComponent as NextMonthArrowButton } from "./icon/arrow_cal_right.svg";
 export { ReactComponent as PrevMonthArrowButton } from "./icon/arrow_cal_left.svg";
+export { ReactComponent as CancelButton } from "./icon/x_btn.svg";

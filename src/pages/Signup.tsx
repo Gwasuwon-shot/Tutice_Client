7129a78d469b873672738f3fbe12968c1c597010
Eunbeann <ds5bean23@gmail.com>
2023-07-12 11:10:57 +0900

    [ feat ] 안쓰는 props 삭제

--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -2,10 +2,9 @@ import { useState } from "react";
 import StepRenderer from "../components/signup/StepRenderer";
 
 export default function Signup() {
-  const [step, setStep] = useState(1);
   return (
     <div>
-      <StepRenderer step={step} setStep={setStep} />
+      <StepRenderer />
     </div>
   );
 }

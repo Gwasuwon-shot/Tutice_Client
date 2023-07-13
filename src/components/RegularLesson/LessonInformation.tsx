7de9c7aacd55fe1ac13bc0084921422a2f217093
parkheeddong <phj7214go@gmail.com>
2023-07-13 12:37:29 +0900

    [ feat ] 수업 정보 컴포넌트 생성

---
+++ src/components/RegularLesson/LessonInformation.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import {RegularLessonStudentIc} from '../../assets';
+import { STUDENT_COLOR } from "../../core/common/studentColor";
+import SubjectLabel from '../common/SubjectLabel';
+import styled from 'styled-components';
+
+export default function LessonInformation() {
+
+
+    
+    return (
+        <LessonInformationWrapper>
+            
+        </LessonInformationWrapper>
+    );
+}
+
+const LessonInformationWrapper = styled.section`
+
+`
--- src/pages/RegularLesson.tsx
+++ src/pages/RegularLesson.tsx
@@ -1,4 +1,5 @@
 import Header from '../components/RegularLesson/Header';
+import LessonInformation from '../components/RegularLesson/LessonInformation';
 import StudentInformation from '../components/RegularLesson/StudentInformation';
 
 export default function RegularLesson() {
@@ -6,6 +7,7 @@ export default function RegularLesson() {
   <>
     <Header />
     <StudentInformation />
+    <LessonInformation />
   </>
   );
 }

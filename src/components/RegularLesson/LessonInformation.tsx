8e9ae5de063be12de13fbff836873ca2561bf8e0
parkheeddong <phj7214go@gmail.com>
2023-07-13 14:39:30 +0900

    [ feat ] 수업 정보 회차, 수업시작일 뷰 추가

--- src/components/RegularLesson/LessonInformation.tsx
+++ src/components/RegularLesson/LessonInformation.tsx
@@ -9,15 +9,98 @@ export default function LessonInformation() {
     
     return (
         <LessonInformationWrapper>
-            <RegularLessonNotebookIcon />
+            <IconWrapper>
+                <RegularLessonNotebookIcon />
+                <SectionName> 수업정보 </SectionName>
+            </IconWrapper>
+            <LessonWrapper>
+                <Turn> 
+                    <TurnName> 회차 </TurnName>
+                    <TurnButton type = "submit"> 회차를 선택하세요 </TurnButton>
+                </Turn>
+                <StartDate>
+                    <StartDateName> 수업시작일 </StartDateName>
+                    <StartDateButton type = "submit"> 2023년 7월 3일 </StartDateButton>
+                </StartDate>
+            </LessonWrapper>
         </LessonInformationWrapper>
+        
     );
 }
 
 const LessonInformationWrapper = styled.section`
+    display: flex;
+    flex-direction: column;
+`
 
+const IconWrapper = styled.div`
+    display: flex;
+    
+    height: 3.1rem;
 `
 
 const RegularLessonNotebookIcon = styled(RegularLessonNotebookIc)`
+    margin-left: 1.7rem;
+    margin-top: 1.5rem;
+`
+
+const SectionName = styled.h1`
+    margin-left: 0.8rem;
+    margin-top: 1.5rem;
     
+    ${({ theme }) => theme.fonts.body04};
+    color: ${({ theme }) => theme.colors.grey600};
+`
+
+const LessonWrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    gap: 1.9rem;
+    
+    width: 28.8rem;
+    height: 9.7rem;
+    margin-left: 1.5rem;
+    padding-left: 0.8rem;
+    
+    border-bottom: 1px solid ${({ theme }) => theme.colors.grey50}
+`
+
+const Turn = styled.div`
+    display: flex;
+`
+
+const TurnName = styled.h2`
+    display: flex;
+    align-items: center;
+    
+    width: 7rem;
+
+    ${({ theme }) => theme.fonts.body04};
+    color: ${({ theme }) => theme.colors.grey400};
+`
+
+const TurnButton = styled.button`
+    ${({ theme }) => theme.fonts.body04};
+    color: ${({ theme }) => theme.colors.grey100};
+    padding: 0;
+`
+
+const StartDate = styled.div`
+    display: flex;
+`
+const StartDateName = styled.h2`
+    display: flex;
+    align-items: center;
+    
+    width: 7rem;
+
+    ${({ theme }) => theme.fonts.body04};
+    color: ${({ theme }) => theme.colors.grey400};
+`
+const StartDateButton = styled.button`
+    padding: 0;
+
+    ${({ theme }) => theme.fonts.body02};
+    color: ${({ theme }) => theme.colors.grey700};
 `

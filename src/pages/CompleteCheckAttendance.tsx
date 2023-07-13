1a9630a4edcc66aebefa2eb327d5a435f5b5cd39
Seojisoo20191941 <amy0480@naver.com>
2023-07-13 16:05:25 +0900

    [ feat ] 로티 이미지 구현

--- src/pages/CompleteCheckAttendance.tsx
+++ src/pages/CompleteCheckAttendance.tsx
@@ -1,10 +1,12 @@
 import Lottie from "lottie-react";
 import check from "../core/checkAttendance/check.json";
+import checkCircle from "../core/checkAttendance/check_circle.json";
 
 export default function CompleteCheckAttendance() {
   return (
     <>
-      <Lottie animationData={check} />
+      <Lottie loop={false} animationData={check} style={{ width: "50%", height: "50%" }} />
+      <Lottie loop={false} animationData={checkCircle} style={{ width: "50%", height: "50%" }} />
     </>
   );
 }

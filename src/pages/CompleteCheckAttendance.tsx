3c9be687862bf311c94663077ccbd12ced60ee19
Seojisoo20191941 <amy0480@naver.com>
2023-07-13 15:00:42 +0900

    [ etc ] 로티 설치

---
+++ src/pages/CompleteCheckAttendance.tsx
@@ -0,0 +1,10 @@
+import Lottie from "lottie-react";
+import check from "../core/checkAttendance/check.json";
+
+export default function CompleteCheckAttendance() {
+  return (
+    <>
+      <Lottie animationData={check} />
+    </>
+  );
+}

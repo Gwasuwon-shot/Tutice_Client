350fd55566d555dd814aa476ee5b9ae0df842134
parkheeddong <phj7214go@gmail.com>
2023-07-13 15:16:39 +0900

    [ feat ] 수업 일시, 요일들 생성

--- src/components/RegularLesson/LessonDate.tsx
+++ src/components/RegularLesson/LessonDate.tsx
@@ -3,7 +3,7 @@ import {RegularLessonClockIc} from '../../assets';
 import styled from 'styled-components';
 
 export default function LessonDate() {
-
+    const days = ['월', '화', '수', '목', '금', '토', '일'];
     return (
         <LessonDateWrapper>
             <IconWrapper>
@@ -11,6 +11,11 @@ export default function LessonDate() {
                 <SectionName> 수업일시 </SectionName>
                 <Explain> 수업종료 5분 뒤에 출결알람을 드릴게요. </Explain>
             </IconWrapper>
+            <DayWrapper>
+                {days.map((day, index) => (
+                <Day key={index}>{day}</Day>
+                ))}
+            </DayWrapper>
         </LessonDateWrapper>
     );
 }
@@ -43,4 +48,21 @@ const Explain = styled.h3`
     margin-left: 1.8rem;
     ${({ theme }) => theme.fonts.caption01};
     color: ${({ theme }) => theme.colors.grey300};  
+`
+
+const DayWrapper = styled.section`
+    display: flex;
+    
+    justify-content: center;
+    gap: 0.2rem;
+    padding-top: 1.2rem;
+`
+
+const Day = styled.button`
+    width: 3.8rem;
+    height: 3.8rem;
+    border-radius: 50%;
+    ${({ theme }) => theme.fonts.body02};
+    color: ${({ theme }) => theme.colors.grey300};  
+    background-color: ${({ theme }) => theme.colors.grey50}; 
 `

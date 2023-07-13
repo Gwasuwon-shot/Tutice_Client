b1ae193ac12256584f545c9bacc93eaa43017e41
parkheeddong <phj7214go@gmail.com>
2023-07-13 11:43:09 +0900

    [ feat ] Header 완성

--- src/components/RegularLesson/Header.tsx
+++ src/components/RegularLesson/Header.tsx
@@ -1,4 +1,5 @@
 import {RegisterLessonHeaderIc} from '../../assets';
+import { TuticeWithTextCommonIc } from "../../assets";
 import styled from 'styled-components';
 
 export default function Header() {
@@ -6,8 +7,9 @@ export default function Header() {
     return (
 
         <HeaderWrapper>
-            <RegisterLessonHeaderIc />
-            정기수업 일정 등록
+            
+            <RegisterLessonHeaderIcon />
+            <HeaderName> 정기수업 일정 등록 </HeaderName>
         </HeaderWrapper>
 
     );
@@ -18,7 +20,20 @@ const HeaderWrapper = styled.header`
     justify-content: center;
     align-items: center;
     
-    height: 3.1rem;
-
+    position: relative;
+    
+    height: 5rem;
+    border-bottom: 1px solid ${({ theme }) => theme.colors.grey50}
 `
 
+const HeaderName = styled.h1`
+    display: flex;
+    
+    ${({ theme }) => theme.fonts.body01};
+    color: ${({ theme }) => theme.colors.grey900};
+`
+const RegisterLessonHeaderIcon = styled(RegisterLessonHeaderIc)`
+    position: absolute;
+    top: 0.8rem;
+    left: 0rem;
+`;

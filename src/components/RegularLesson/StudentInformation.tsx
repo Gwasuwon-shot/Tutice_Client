a2061a6873d74d8d850f17d79791e1d1486a668a
parkheeddong <phj7214go@gmail.com>
2023-07-13 12:34:45 +0900

    [ feat ] 학생정보 추가

--- src/components/RegularLesson/StudentInformation.tsx
+++ src/components/RegularLesson/StudentInformation.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import {RegularLessonStudentIc} from '../../assets';
+import { STUDENT_COLOR } from "../../core/common/studentColor";
+import SubjectLabel from '../common/SubjectLabel';
 import styled from 'styled-components';
 
 export default function StudentInformation() {
 
+    const idx = 1;                  // 임시 Idx
+    const subject = "수학";         // 임시 Subject
+    
     return (
         <StudentInformationWrapper >
             <IconWrapper>
@@ -11,10 +16,10 @@ export default function StudentInformation() {
                 <SectionName> 학생정보 </SectionName>
             </IconWrapper>
             <StudentWrapper>
-                
+                <StudentName> 박송현 </StudentName>
+                <SubjectLabel subject={subject} backgroundColor={STUDENT_COLOR[idx % 11]} color="#5B6166" />
             </StudentWrapper>
         </StudentInformationWrapper>
-
     );
 }
 
@@ -43,5 +48,20 @@ const SectionName = styled.h1`
 `
 
 const StudentWrapper = styled.div`
+    display: flex;
+    align-items: center;
+    
+    width: 28.8rem;
+    height: 5rem;
+    margin-left: 1.5rem;
 
+    border-bottom: 1px solid ${({ theme }) => theme.colors.grey50}
+`
+
+const StudentName = styled.h2`
+    margin-left: 0.8rem;
+    margin-right: 0.4rem;
+    
+    ${({ theme }) => theme.fonts.body01};
+    color: ${({ theme }) => theme.colors.grey700};
 `

d611e04db81477d23f5761993a01bbefd54a1a7f
HAE2NI <hae0_02ni@ewhain.net>
2023-07-13 14:27:55 +0900

    [ etc ] parents calendar 수정

--- src/core/Parents/ParentsCalendar.ts
+++ src/core/Parents/ParentsCalendar.ts
@@ -2,52 +2,56 @@ export const PARENTS_CALENDAR = {
   code: 200,
   message: "스케줄 메인 뷰 조회 성공",
   data: {
-    lessonList: [
+    scheduleList: [
       {
-        lesson: {
-          idx: 1,
-          studentName: "박송현",
-          subject: "수학",
-        },
-        scheduleList: [
+        date: "2023-07-04",
+        dayOfWeek: "화",
+        dailyScheduleList: [
           {
-            idx: 1,
-            date: "2023-07-04",
-            startTime: "17:00",
-            endTime: "19:00",
+            lessonIdx: 1,
+            schedule: {
+              idx: 1,
+              studentName: "박송현",
+              subject: "수학",
+              startTime: "17:00",
+              endTime: "19:00",
+            },
           },
           {
-            idx: 2,
-            date: "2023-07-07",
-            startTime: "17:00",
-            endTime: "19:00",
+            lessonIdx: 2,
+            schedule: {
+              idx: 2,
+              studentName: "이유나",
+              subject: "영어",
+              startTime: "19:00",
+              endTime: "21:00",
+            },
           },
           {
-            idx: 3,
-            date: "2023-07-11",
-            startTime: "17:00",
-            endTime: "19:00",
+            lessonIdx: 3,
+            schedule: {
+              idx: 3,
+              studentName: "조판다",
+              subject: "국어",
+              startTime: "21:00",
+              endTime: "23:00",
+            },
           },
         ],
       },
       {
-        lesson: {
-          idx: 2,
-          studentName: "이유나",
-          subject: "영어",
-        },
-        scheduleList: [
+        date: "2023-07-07",
+        dayOfWeek: "금",
+        dailyScheduleList: [
           {
-            idx: 4,
-            date: "2023-07-04",
-            startTime: "15:00",
-            endTime: "21:00",
-          },
-          {
-            idx: 5,
-            date: "2023-07-07",
-            startTime: "19:00",
-            endTime: "21:00",
+            lessonIdx: 1,
+            schedule: {
+              idx: 1,
+              studentName: "박송현",
+              subject: "수학",
+              startTime: "17:00",
+              endTime: "19:00",
+            },
           },
         ],
       },

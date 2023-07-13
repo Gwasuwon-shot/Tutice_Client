808303c8a6097c438a3ad83123cc165eceae4312
Seojisoo20191941 <amy0480@naver.com>
2023-07-13 21:23:24 +0900

    [ design ] 공통 컴포넌트 수정

--- src/components/common/StudentNameLabel.tsx
+++ src/components/common/StudentNameLabel.tsx
@@ -6,22 +6,23 @@ interface StudentNameLabelProps {
   subject: string;
   backgroundColor: string;
   color: string;
+  isBig: boolean;
 }
 
 export default function StudentNameLabel(props: StudentNameLabelProps) {
-  const { studentName, subject, backgroundColor, color } = props;
+  const { studentName, subject, backgroundColor, color, isBig } = props;
 
   return (
     <StudentNameWrapper>
-      <StudentName>{studentName}</StudentName>
+      <StudentName $isBig={isBig}>{studentName}</StudentName>
       <Student>학생</Student>
       <SubjectLabel subject={subject} backgroundColor={backgroundColor} color={color} />
     </StudentNameWrapper>
   );
 }
 
-const StudentName = styled.h1`
-  ${({ theme }) => theme.fonts.title01};
+const StudentName = styled.h1<{ $isBig: boolean }>`
+  ${({ theme, $isBig }) => ($isBig ? theme.fonts.title01 : theme.fonts.title02)};
 `;
 
 const StudentNameWrapper = styled.header`
@@ -29,9 +30,9 @@ const StudentNameWrapper = styled.header`
   justify-content: space-between;
   align-items: center;
 
-  width: 10.8rem;
+  width: ${({ $isBig }) => ($isBig ? 12 : 10.8)}rem;
 `;
 
 const Student = styled.p`
-  ${({ theme }) => theme.fonts.title02};
+  ${({ theme }) => theme.fonts.title03};
 `;
--- src/pages/ManageLesson.tsx
+++ src/pages/ManageLesson.tsx
@@ -1,7 +1,23 @@
+import { useRecoilState } from "recoil";
+import { upcomingClassData } from "../atom/attendanceCheck/upcomingClassData";
+import StudentNameLabel from "../components/common/StudentNameLabel";
+import { STUDENT_COLOR } from "../core/common/studentColor";
+
 export default function ManageLesson() {
+  const [classData, setclassData] = useRecoilState(upcomingClassData);
+  const { todaySchedule } = classData;
+  const { lesson, schedule } = todaySchedule;
+  const { idx, studentName, subject } = lesson;
+  const { count, isLastCount } = schedule;
+
   return (
     <>
-      <StudentNameLabel />
+      <StudentNameLabel
+        studentName={studentName}
+        subject={subject}
+        backgroundColor={STUDENT_COLOR[idx % 11]}
+        color="#757A80"
+      />
     </>
   );
 }

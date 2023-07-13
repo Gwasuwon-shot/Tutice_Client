3ce215ef274432a0c4cbafb58f3b699d94783ac6
Seojisoo20191941 <amy0480@naver.com>
2023-07-14 02:26:20 +0900

    [ style ] 데이터 패칭 커스텀 훅으로 준비

--- src/hooks/useManageLesson.ts
+++ src/hooks/useManageLesson.ts
@@ -2,7 +2,7 @@ import { LESSON_SCHEDULE } from "../core/checkAttendance/lessonSchedule";
 
 export default function useManageLesson() {
   //   api 패칭
-  const data = LESSON_SCHEDULE?.data;
+  const { lesson, scheduleList } = LESSON_SCHEDULE?.data;
 
-  return { data };
+  return { lesson, scheduleList };
 }

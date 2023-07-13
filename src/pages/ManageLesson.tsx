1c6b764d79572ea150cf3642b43dfd88708c9cd5
Seojisoo20191941 <amy0480@naver.com>
2023-07-14 02:34:40 +0900

    [ style ] 컴포넌트 쪼개기

--- src/pages/ManageLesson.tsx
+++ src/pages/ManageLesson.tsx
@@ -1,29 +1,15 @@
 import { styled } from "styled-components";
 import BackButton from "../components/common/BackButton";
-import StudentNameLabel from "../components/common/StudentNameLabel";
 import ManageLessonCategory from "../components/manageLesson/ManageLessonCategory";
+import StudentNameBox from "../components/manageLesson/StudentNameBox";
 import TreeLevel from "../components/manageLesson/TreeLevel";
-import { LESSON_SCHEDULE } from "../core/checkAttendance/lessonSchedule";
-import { STUDENT_COLOR } from "../core/common/studentColor";
 
 export default function ManagfeLesson() {
-  // 서버에서 가져올 데이터
-  const { lesson, scheduleList } = LESSON_SCHEDULE?.data;
-  const { idx, studentName, subject, count, nowCount } = lesson;
-
   return (
     <>
       <BackButton />
       <ManageLessonWrapper>
-        <LabelWrapper>
-          <StudentNameLabel
-            studentName={studentName}
-            subject={subject}
-            backgroundColor={STUDENT_COLOR[idx % 11]}
-            color="#757A80"
-            isBig={true}
-          />
-        </LabelWrapper>
+        <StudentNameBox />
         <ManageLessonCategory />
         <TreeLevel />
       </ManageLessonWrapper>
@@ -34,8 +20,4 @@ export default function ManagfeLesson() {
 const ManageLessonWrapper = styled.div`
   padding: 0 1.4rem;
   margin-top: 1rem;
-`;
-
-const LabelWrapper = styled.header`
-  margin-left: 0.4rem;
 `;

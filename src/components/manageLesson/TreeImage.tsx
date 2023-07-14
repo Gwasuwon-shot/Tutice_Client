7a0afad59c33824b896f96938e0ac701dcf308df
Seojisoo20191941 <amy0480@naver.com>
2023-07-15 05:54:12 +0900

    [ feat ] 수업 퍼센트에 따라 열매 이미지 가져오기 구현

--- src/components/manageLesson/TreeImage.tsx
+++ src/components/manageLesson/TreeImage.tsx
@@ -1,4 +1,5 @@
 import { styled } from "styled-components";
+import { LESSON_STATUS_IMAGE } from "../../core/manageLesson/lessonStatusImage";
 import useManageLesson from "../../hooks/useManageLesson";
 import TreeProgress from "../common/TreeProgress";
 
@@ -6,9 +7,26 @@ export default function TreeImage() {
   const { lesson } = useManageLesson();
   const { count, nowCount, percent } = lesson;
 
+  function checkTreeSrc() {
+    switch (true) {
+      case percent > 80:
+        return LESSON_STATUS_IMAGE.level5;
+      case percent > 60:
+        return LESSON_STATUS_IMAGE.level4;
+      case percent > 40:
+        return LESSON_STATUS_IMAGE.level3;
+      case percent > 20:
+        return LESSON_STATUS_IMAGE.level2;
+      case percent > 0:
+        return LESSON_STATUS_IMAGE.level1;
+      default:
+        return;
+    }
+  }
+
   return (
     <>
-      {/* 이미지 넣기 */}
+      <img src={checkTreeSrc()} alt="열매 이미지" />
       <CountBox>
         {count - nowCount}회/ 총 {count}회
       </CountBox>

788d1837fe3a004f4ad9ff0c2046b640da965214
Eunbeann <ds5bean23@gmail.com>
2023-07-14 13:27:35 +0900

    [ feat ] 로그인 텍스트필드 구현

--- src/components/login/LoginInput.tsx
+++ src/components/login/LoginInput.tsx
@@ -1,5 +1,35 @@
 import React from "react";
+import { styled } from "styled-components";
+import TextLabelLayout from "../signup/TextLabelLayout";
 
 export default function LoginInput() {
-  return <div></div>;
+  return (
+    <>
+      <InputFieldWrapper>
+        <TextLabelLayout labelText="이메일" />
+        <Inputfield type="text" placeholder="이메일을 입력하세요." />
+      </InputFieldWrapper>
+      <InputFieldWrapper>
+        <TextLabelLayout labelText="비밀번호" />
+        <Inputfield type="password" placeholder="비밀번호를 입력하세요" />
+      </InputFieldWrapper>
+    </>
+  );
 }
+
+const InputFieldWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  margin-bottom: 2rem;
+  border-bottom: 0.1rem solid ${({ theme }) => theme.colors.grey70};
+`;
+
+const Inputfield = styled.input`
+  margin: 1rem 0.2rem;
+
+  &::placeholder {
+    color: ${({ theme }) => theme.colors.grey400};
+    ${({ theme }) => theme.fonts.title03};
+  }
+`;

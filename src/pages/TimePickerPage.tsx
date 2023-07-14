e3366be807ea461d1e67a2dd08285366cfd1aaf8
parkheeddong <phj7214go@gmail.com>
2023-07-14 23:16:32 +0900

    [ feat ] DatePicker 생성

--- src/pages/TimePickerPage.tsx
+++ src/pages/TimePickerPage.tsx
@@ -1,8 +1,20 @@
+import DatePicker from '../components/RegularLesson/TimePicker/DatePicker';
 import TimePicker from '../components/RegularLesson/TimePicker/TimePicker';
+import {styled} from 'styled-components';
 import { useState } from "react";
 
 export default function TimePickerPage() {
   return (
-    <TimePicker />
+    <PickerPage>
+      <TimePicker />
+      <DatePicker />
+    </PickerPage>
   );
 }
+
+
+const PickerPage = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 2rem;
+`

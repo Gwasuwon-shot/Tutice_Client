2a0d7769bc59f850d5da52f57817fb6902a75e7c
Seojisoo20191941 <amy0480@naver.com>
2023-07-15 04:03:32 +0900

    [ feat ] 입금완료된 리스트에 미입금 리스트 더하기 구현

--- src/components/manageLesson/StudentPayment.tsx
+++ src/components/manageLesson/StudentPayment.tsx
@@ -1,3 +1,5 @@
+import { PAYMENT_STATUS_IMAGE } from "../../core/manageLesson/paymentStatusImage";
+
 interface StudentPaymentProps {
   idx: number;
   date: string;
@@ -7,5 +9,17 @@ interface StudentPaymentProps {
 export default function StudentPayment(props: StudentPaymentProps) {
   const { idx, date, amount } = props;
 
-  return <div>StudentPayment</div>;
+  function checkPaymentDone() {
+    return amount >= 0;
+  }
+
+  return (
+    <>
+      {checkPaymentDone() ? (
+        <img src={PAYMENT_STATUS_IMAGE.done} alt="입금 완료 열매" />
+      ) : (
+        <img src={PAYMENT_STATUS_IMAGE.notYet} alt="입금 미완료 열매" />
+      )}
+    </>
+  );
 }
--- src/components/manageLesson/StudentPayments.tsx
+++ src/components/manageLesson/StudentPayments.tsx
@@ -1,21 +1,32 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import useGetAllPayments from "../../hooks/useGetAllPayments";
 import StudentPayment from "./StudentPayment";
+
+interface PaymentRecordsType {
+  idx: number;
+  date: string;
+  amount: number;
+}
 
 export default function StudentPayments() {
   const { lesson, todayDate, paymentRecord } = useGetAllPayments();
   const { cycle } = lesson;
   const { paymentRecordList } = paymentRecord;
-  const allPaymentRecords = [...paymentRecordList];
+  const [allPaymentRecords, setAllPaymentRecords] = useState<PaymentRecordsType[]>([]);
 
   useEffect(() => {
     // 이미 입금완료된 리스트만 있는 paymentRecordList에 입금이 필요한 리스트를 채워넣는 작업
     if (paymentRecordList.length < cycle) {
+      const copyPaymentRecordList = [...paymentRecordList];
       new Array(cycle - paymentRecordList.length)
         .fill(0)
-        .map((idx) => allPaymentRecords.unshift({ idx: idx, date: `${todayDate}`, amount: -1 }));
+        .forEach((val, index) => copyPaymentRecordList.unshift({ idx: index, date: `${todayDate}`, amount: -1 }));
+
+      setAllPaymentRecords(copyPaymentRecordList);
     }
   }, []);
+
+  console.log(allPaymentRecords);
 
   return (
     <>

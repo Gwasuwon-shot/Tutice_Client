690ccfd7d6950e4fa177fb64dc263401e6b60141
Seojisoo20191941 <amy0480@naver.com>
2023-07-15 05:57:50 +0900

    [ design ] 푸터 만큼 넉넉하게 페이지 바틈 값 주기

--- src/components/manageLesson/AttendanceInforms.tsx
+++ src/components/manageLesson/AttendanceInforms.tsx
@@ -20,16 +20,18 @@ export default function AttendanceList() {
         {openModal && <AttendanceCheckModal setIsCheckingModalOpen={setIsCheckingModalOpen} />}
       </ModalWrapper>
       <GreyBox />
-      {scheduleList.map(({ idx, date, status, startTime, endTime }, index) => (
-        <AttendanceInform
-          key={idx}
-          date={date}
-          status={status}
-          startTime={startTime}
-          endTime={endTime}
-          count={Math.abs(index - scheduleList.length)}
-        />
-      ))}
+      <ScheduleWrapper>
+        {scheduleList.map(({ idx, date, status, startTime, endTime }, index) => (
+          <AttendanceInform
+            key={idx}
+            date={date}
+            status={status}
+            startTime={startTime}
+            endTime={endTime}
+            count={Math.abs(index - scheduleList.length)}
+          />
+        ))}
+      </ScheduleWrapper>
     </>
   );
 }
@@ -46,4 +48,10 @@ const ModalWrapper = styled.section`
   position: absolute;
 
   margin: -20.1rem 0 0 -1.5rem;
+`;
+
+const ScheduleWrapper = styled.section`
+  overflow: scroll;
+
+  padding-bottom: 7.2rem;
 `;

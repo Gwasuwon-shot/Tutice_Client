744a87b7531a48eaae492f69f46c5c5182678239
Seojisoo20191941 <amy0480@naver.com>
2023-07-15 05:02:54 +0900

    [ design ] 위치 조정

--- src/components/manageLesson/AttnedanceInform.tsx
+++ src/components/manageLesson/AttnedanceInform.tsx
@@ -87,6 +87,7 @@ const StatusLabel = styled.label<{ $status: string }>`
 
   width: 7.4rem;
   height: 4rem;
+  padding-right: 1rem;
 
   color: ${({ theme, $status }) =>
     $status === ATTENDANCE_STATUS.attend

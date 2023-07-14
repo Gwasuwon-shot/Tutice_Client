de12dfdf53956c65338bb6ba5daa285f38e64f5a
Eunbeann <ds5bean23@gmail.com>
2023-07-14 15:08:34 +0900

    [ feat ] 입력시 밑줄 활성화;

--- src/components/login/LoginInput.tsx
+++ src/components/login/LoginInput.tsx
@@ -4,43 +4,83 @@ import TextLabelLayout from "../signup/TextLabelLayout";
 import { ViewingLoginIc } from "../../assets";
 
 export default function LoginInput() {
-  const [pwViewing, setPwViewing] = useState(false);
+  // 임시 recoil로 만들어라
+  const [userLogin, setUserLogin] = useState([]);
+  const [email, setEmail] = useState("");
+  const [emailFocus, setEmailFocus] = useState(false);
+  const [password, setPassword] = useState("");
   const [pwFocus, setPwFocus] = useState(false);
-  const [password, setPassword] = useState(false);
+  const [pwViewing, setPwViewing] = useState(false);
+  const [isActive, setIsActive] = useState(false);
 
-  function handlePWFocusing(e) {
+  // setEmail
+  function handelEmailChange(e: React.ChangeEvent<HTMLInputElement>) {
+    setEmail(e.target.value);
+  }
+
+  //setPassword
+  function handlePasswordChange(e: React.ChangeEvent<HTMLInputElement>) {
     setPassword(e.target.value);
+  }
+
+  //데이터 전달
+  function handleLoginClick() {
+    setUserLogin((prev) => ({ ...prev, email: email, password: password }));
   }
 
   useEffect(() => {
     console.log(password);
-    password === "" ? setPwViewing(false) : setPwViewing(true);
+    // 버튼 활성화
+
+    // 비밀번호 뷰잉 활성화
+    password ? setPwViewing(false) : setPwViewing(true);
   }, []);
 
   return (
     <>
-      <InputFieldWrapper>
+      <InputEmailWrapper $emailFocus={emailFocus}>
         <TextLabelLayout labelText="이메일" />
-        <Inputfield type="text" placeholder="이메일을 입력하세요." />
-      </InputFieldWrapper>
-      <InputFieldWrapper>
+        <Inputfield
+          onFocus={() => setEmailFocus(true)}
+          onChange={(e) => handelEmailChange(e)}
+          type="text"
+          placeholder="이메일을 입력하세요."
+        />
+      </InputEmailWrapper>
+      <InputPasswordWrapper $pwFocus={pwFocus}>
         <TextLabelLayout labelText="비밀번호" />
         <PasswordIconWrapper>
-          <Inputfield onChange={(e) => handlePWFocusing(e)} type="password" placeholder="비밀번호를 입력하세요" />
+          <Inputfield
+            onFocus={() => setPwFocus(true)}
+            onChange={(e) => handlePasswordChange(e)}
+            type="password"
+            placeholder="비밀번호를 입력하세요"
+          />
           {pwViewing ? <ViewingLoginIcon /> : null}
         </PasswordIconWrapper>
-      </InputFieldWrapper>
+      </InputPasswordWrapper>
     </>
   );
 }
 
-const InputFieldWrapper = styled.div`
+const InputEmailWrapper = styled.div`
   display: flex;
   flex-direction: column;
 
   margin-right: 1.4rem;
   margin-bottom: 2rem;
-  border-bottom: 0.1rem solid ${({ theme }) => theme.colors.grey70};
+
+  border-bottom: 0.1rem solid ${({ theme, $emailFocus }) => ($emailFocus ? theme.colors.green5 : theme.colors.grey70)};
+`;
+
+const InputPasswordWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  margin-right: 1.4rem;
+  margin-bottom: 2rem;
+
+  border-bottom: 0.1rem solid ${({ theme, $pwFocus }) => ($pwFocus ? theme.colors.green5 : theme.colors.grey70)};
 `;
 
 const Inputfield = styled.input`

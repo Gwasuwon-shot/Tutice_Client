c6afde733f2204466274f5d67b8e46ff91125c03
Seojisoo20191941 <amy0480@naver.com>
2023-07-14 16:56:20 +0900

    [ feat ] 학생 탭 구현

--- src/components/manageLesson/MainLesson.tsx
+++ src/components/manageLesson/MainLesson.tsx
@@ -1,3 +1,26 @@
-export default function MainLesson() {
-  return <div>MainLesson</div>;
+import { STUDENT_COLOR } from "../../core/common/studentColor";
+import StudentColorBox from "../common/StudentColorBox";
+import SubjectLabel from "../common/SubjectLabel";
+import TreeProgress from "../common/TreeProgress";
+
+interface MainLessonProps {
+  idx: number;
+  studentName: string;
+  subject: string;
+  percent: number;
+  dayOfWeekList: string[];
+}
+
+export default function MainLesson(props: MainLessonProps) {
+  const { idx, studentName, subject, percent, dayOfWeekList } = props;
+
+  return (
+    <>
+      <StudentColorBox backgroundColor={STUDENT_COLOR[idx % 11]} />
+      <h1>{studentName}</h1>
+      <SubjectLabel subject={subject} backgroundColor={STUDENT_COLOR[idx % 11]} color="#5B6166" />
+      {dayOfWeekList.map((day) => day)}
+      <TreeProgress progress={percent} />
+    </>
+  );
 }
--- src/components/manageLesson/MainLessons.tsx
+++ src/components/manageLesson/MainLessons.tsx
@@ -1,8 +1,22 @@
+import useGetAllLessons from "../../hooks/useGetAllLessons";
+import MainLesson from "./MainLesson";
+
 export default function MainLessons() {
+  const { lessonList } = useGetAllLessons();
+
   return (
     <>
       <h1>나의 수업</h1>
-      {}
+      {lessonList.map(({ idx, studentName, subject, percent, dayOfWeekList }) => (
+        <MainLesson
+          key={idx}
+          idx={idx}
+          studentName={studentName}
+          subject={subject}
+          percent={percent}
+          dayOfWeekList={dayOfWeekList}
+        />
+      ))}
     </>
   );
 }

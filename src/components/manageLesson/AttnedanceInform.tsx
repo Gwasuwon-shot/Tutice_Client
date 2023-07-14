d026d99f97cf0649676a1ff340fd2bec66aa9d78
Seojisoo20191941 <amy0480@naver.com>
2023-07-14 15:04:33 +0900

    [ feat ] 수업 리스트 박스 구현

--- src/components/manageLesson/AttnedanceInform.tsx
+++ src/components/manageLesson/AttnedanceInform.tsx
@@ -8,5 +8,18 @@ interface AttendanceInformProps {
 export default function AttnedanceInform(props: AttendanceInformProps) {
   const { date, status, startTime, endTime } = props;
 
-  return <div>AttnedanceInform</div>;
+  return (
+    <article>
+      <p>
+        {date.split("-")[1]}.{date.split("-")[2]}
+      </p>
+      <div>
+        <h1>회차 수업</h1>
+        <p>
+          {startTime} ~ {endTime}
+        </p>
+      </div>
+      <p>{status}</p>
+    </article>
+  );
 }

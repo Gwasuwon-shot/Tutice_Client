6f5087f1a434311a989bf1a36d2e5ac08a4e904f
Eunbeann <ds5bean23@gmail.com>
2023-07-15 21:12:03 +0900

    [ etc ] 리스트 core로 빼기

--- src/components/signup/AgreeChecking.tsx
+++ src/components/signup/AgreeChecking.tsx
@@ -4,35 +4,7 @@ import { TosNoneSignupIc } from "../../assets";
 import { TosCheckedSignupIc } from "../../assets";
 import { useSetRecoilState } from "recoil";
 import { newUserData } from "../../atom/signup/signup";
-
-interface checkListProps {
-  id: number;
-  selected: boolean;
-}
-
-const checkList: checkListProps[] = [
-  { id: 0, selected: false },
-  { id: 1, selected: false },
-  { id: 2, selected: false },
-  { id: 3, selected: false },
-  { id: 4, selected: false },
-];
-
-interface textListProps {
-  id: number;
-  optional: string;
-  boldText: string;
-  linkText: string;
-  lightText: string;
-}
-
-const textList: textListProps[] = [
-  { id: 0, optional: "", boldText: "약관 전체 동의", linkText: "", lightText: "선택항목에 대한 동의 포함" },
-  { id: 1, optional: "(필수)", boldText: "만 14세 이상입니다.", linkText: "", lightText: "" },
-  { id: 2, optional: "(필수)", boldText: "동의", linkText: "서비스 이용 약관", lightText: "" },
-  { id: 3, optional: "(필수)", boldText: "동의", linkText: "개인정보 수집 및 이용", lightText: "" },
-  { id: 4, optional: "(선택)", boldText: "동의", linkText: "개인정보 마케팅 활용", lightText: "" },
-];
+import { checkList, textList } from "../../core/Login/ListData";
 
 export default function AgreeChecking() {
   const setNewUser = useSetRecoilState(newUserData);

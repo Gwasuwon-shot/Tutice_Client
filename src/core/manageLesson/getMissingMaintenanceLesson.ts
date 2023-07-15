db26ab53298e507688687820db315174ffeb4ecb
HAE2NI <hae0_02ni@ewhain.net>
2023-07-16 03:36:29 +0900

    [ etc ] 데이터 추가

---
+++ src/core/manageLesson/getMissingMaintenanceLesson.ts
@@ -0,0 +1,35 @@
+export const MISSING_MAINTEANANCE_LESSON = {
+  code: 200,
+  message: "수업 연장여부로 다음 사이클 스케쥴 자동생성",
+  data: {
+    missingMaintenanceLessonList: [
+      {
+        lesson: {
+          idx: 34,
+          studentName: "박송현",
+          subject: "자유형",
+          count: 7,
+        },
+        endScheduleDate: "2023-05-25",
+      },
+      {
+        lesson: {
+          idx: 34,
+          studentName: "박송현",
+          subject: "자유형",
+          count: 7,
+        },
+        endScheduleDate: "2023-06-25",
+      },
+      {
+        lesson: {
+          idx: 34,
+          studentName: "박송현",
+          subject: "자유형",
+          count: 7,
+        },
+        endScheduleDate: "2023-08-25",
+      },
+    ],
+  },
+};

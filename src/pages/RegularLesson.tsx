81ef008600e4e1b9fc06bdf414ec5938a1723e8c
parkheeddong <phj7214go@gmail.com>
2023-07-15 19:36:01 +0900

    [ feat ] recoil로 이름, 과목 받아와서 표시

--- src/pages/RegularLesson.tsx
+++ src/pages/RegularLesson.tsx
@@ -1,11 +1,13 @@
 import Header from '../components/RegularLesson/Header';
 import LessonDate from '../components/RegularLesson/LessonDate';
 import LessonInformation from '../components/RegularLesson/LessonInformation';
+import ProgressBar from "../components/common/ProgressBar";
 import StudentInformation from '../components/RegularLesson/StudentInformation';
 
 export default function RegularLesson() {
   return (
   <>
+    <ProgressBar progress = {50} />
     <Header />
     <StudentInformation />
     <LessonInformation />

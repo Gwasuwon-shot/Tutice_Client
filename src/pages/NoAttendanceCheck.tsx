63c5703a57f0d615b98620d55897f16538f15d25
HAE2NI <hae0_02ni@ewhain.net>
2023-07-15 18:58:59 +0900

    [ feat ] 놓친 출결 페이지

---
+++ src/pages/NoAttendanceCheck.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import NoAttendanceheader from "../components/noAttendance/NoAttendanceheader";
+import NoCheckLesson from "../components/noAttendance/noCheckLesson";
+
+export default function NoAttendanceCheck() {
+  return (
+    <>
+      <NoAttendanceheader></NoAttendanceheader>
+      <NoCheckLesson />
+    </>
+  );
+}

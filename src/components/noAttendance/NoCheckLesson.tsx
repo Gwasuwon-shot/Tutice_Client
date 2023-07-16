33cddb89671447daf86b910f9f611a0c44faf37c
HAE2NI <hae0_02ni@ewhain.net>
2023-07-16 12:28:40 +0900

    [ feat ] customhook + component 분리

--- src/components/noAttendance/NoCheckLesson.tsx
+++ src/components/noAttendance/NoCheckLesson.tsx
@@ -1,10 +1,7 @@
 import React from "react";
 import { styled } from "styled-components";
-import { NO_ATTENDNACE_CHECK } from "../../core/noAttendanceCheck/noAttendance";
-import StudentColorBox from "../common/StudentColorBox";
-import { STUDENT_COLOR } from "../../core/common/studentColor";
-import SubjectLabel from "../common/SubjectLabel";
-import NoCheckPageAttendanceButton from "../common/NoCheckPageAttendanceButton";
+import useNoAttendance from "../../hooks/useNoAttendance";
+import NoCheckAttendanceContanier from "./NoCheckAttendanceContanier";
 
 interface LessonData {
   idx: number;
@@ -28,7 +25,7 @@ interface MissingAttendanceData {
 }
 
 export default function NoCheckLesson() {
-  const { missingAttendanceDateList } = NO_ATTENDNACE_CHECK.data;
+  const missingAttendanceDateList = useNoAttendance();
 
   return (
     <>
@@ -48,25 +45,14 @@ export default function NoCheckLesson() {
                 const { startTime, endTime, count } = schedule;
 
                 return (
-                  <ContentContainer>
-                    <StudentColorBox backgroundColor={STUDENT_COLOR[idx % 11]} />
-                    <InforContainer>
-                      <TimeWrapper>
-                        <Time>
-                          {startTime} ~ {endTime}
-                        </Time>
-                        <Bar> | </Bar>
-                        <Time>{count}회차</Time>
-                      </TimeWrapper>
-                      <NameSubjectWrapper>
-                        <Name> {studentName}</Name>
-                        <Subject>
-                          <SubjectLabel subject={subject} backgroundColor={STUDENT_COLOR[idx % 11]}></SubjectLabel>
-                        </Subject>
-                      </NameSubjectWrapper>
-                    </InforContainer>
-                    <NoCheckPageAttendanceButton />
-                  </ContentContainer>
+                  <NoCheckAttendanceContanier
+                    idx={idx}
+                    studentName={studentName}
+                    subject={subject}
+                    startTime={startTime}
+                    endTime={endTime}
+                    count={count}
+                  />
                 );
               })}
             </NoAttendanceContainer>
@@ -98,58 +84,4 @@ const NoAttendanceDate = styled.div`
 
   width: 100%;
   ${({ theme }) => theme.fonts.body04};
-`;
-const ContentContainer = styled.div`
-  display: flex;
-  gap: 1.4rem;
-`;
-
-const InforContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 0.4rem;
-
-  margin-right: 6rem;
-`;
-
-const Time = styled.h2`
-  ${({ theme }) => theme.fonts.body06};
-  color: ${({ theme }) => theme.colors.grey500};
-`;
-
-const TimeWrapper = styled.div`
-  display: flex;
-
-  height: 1.3rem;
-
-  gap: 0.5rem;
-`;
-
-const Name = styled.h2`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-
-  ${({ theme }) => theme.fonts.body01};
-  color: ${({ theme }) => theme.colors.grey900};
-`;
-
-const Subject = styled.h3`
-  ${({ theme }) => theme.fonts.caption01};
-  color: ${({ theme }) => theme.colors.grey500};
-
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
-
-const Bar = styled.p`
-  color: ${({ theme }) => theme.colors.grey100};
-`;
-
-const NameSubjectWrapper = styled.div`
-  display: flex;
-
-  height: 1.6rem;
-  gap: 0.4rem;
 `;

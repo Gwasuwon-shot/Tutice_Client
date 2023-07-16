5d03cecf7d6735eb6a52cf1e147303b73c63c26d
Seojisoo20191941 <amy0480@naver.com>
2023-07-16 20:31:57 +0900

    [ style ] 리코일을 커스텀 훅으로 변경

--- src/components/common/AttendanceCheckModal.tsx
+++ src/components/common/AttendanceCheckModal.tsx
@@ -1,10 +1,10 @@
 import { useRecoilState } from "recoil";
 import styled from "styled-components";
 import { attendanceStatus } from "../../atom/attendanceCheck/attendanceStatus";
-import { upcomingClassData } from "../../atom/attendanceCheck/upcomingClassData";
 import { isModalOpen } from "../../atom/common/isModalOpen";
 import { ATTENDANCE_STATUS } from "../../core/common/attendanceStatus";
 import { STUDENT_COLOR } from "../../core/common/studentColor";
+import useGetTodayScheduleByTeacher from "../../hooks/useGetTodayScheduleByTeacher";
 import AttendanceStatusButton from "./AttendanceStatusButton";
 import SubjectLabel from "./SubjectLabel";
 import ToastModal from "./ToastModal";
@@ -15,8 +15,7 @@ interface AttendanceCheckModalProp {
 
 export default function AttendanceCheckModal(props: AttendanceCheckModalProp) {
   const { setIsCheckingModalOpen } = props;
-  const [classData, setclassData] = useRecoilState(upcomingClassData);
-  const { todaySchedule } = classData;
+  const { teacherName, isTodaySchedule, todaySchedule } = useGetTodayScheduleByTeacher();
   const { lesson, schedule } = todaySchedule;
   const { idx, studentName, subject } = lesson;
   const { count, isLastCount } = schedule;

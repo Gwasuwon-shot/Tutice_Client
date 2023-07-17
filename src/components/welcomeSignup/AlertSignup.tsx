6f0a8ad4c5fa1b889e44be2057b42833a93efea7
Eunbeann <ds5bean23@gmail.com>
2023-07-17 20:54:55 +0900

    [ feat ] AlertSignup 뷰 끝

--- src/components/welcomeSignup/AlertSignup.tsx
+++ src/components/welcomeSignup/AlertSignup.tsx
@@ -5,11 +5,12 @@ import SignupTitleLayout from "../signup/SignupTitleLayout";
 export default function AlertSignup() {
   const MAIN_TEXT = `수업 나무를 통한 \n 쉬운 관리를 위해\n 알림을 활성화 해보세요 `;
 
-  const SUB_TEXT = "*수업나무 : 수강생을 추가하여 관리할 수 있는 서비스입니다.";
+  const SUB_TEXT = "푸시알림을 활성화를 통해 \n 출결, 수업비 관리를 도울 수 있어요";
   return (
     <Container>
       <BellWelcomeIcon />
       <SignupTitleLayout MainText={MAIN_TEXT} />
+      <SubText>{SUB_TEXT}</SubText>
     </Container>
   );
 }
@@ -21,4 +22,11 @@ const BellWelcomeIcon = styled(bellWelcomeIc)`
   width: 2.9rem;
   height: 3.3rem;
   margin-bottom: 1.5rem;
+`;
+
+const SubText = styled.p`
+  margin-top: 2.2rem;
+
+  color: ${({ theme }) => theme.colors.grey600};
+  ${({ theme }) => theme.fonts.body04};
 `;

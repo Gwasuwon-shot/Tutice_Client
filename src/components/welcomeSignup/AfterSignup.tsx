5667701ff7aaca470950b5521f63424fc1a1f217
Eunbeann <ds5bean23@gmail.com>
2023-07-17 19:35:23 +0900

    [ feat ] 삼항 연산자로 렌더링 처리

--- src/components/welcomeSignup/AfterSignup.tsx
+++ src/components/welcomeSignup/AfterSignup.tsx
@@ -1,5 +1,18 @@
-import React from "react";
+import { styled } from "styled-components";
+import { TuticeWithTextCommonIc } from "../../assets";
 
 export default function AfterSignup() {
-  return <div></div>;
+  return (
+    <div>
+      <TuticeWithTextCommonIcon />
+    </div>
+  );
 }
+
+const TuticeWithTextCommonIcon = styled(TuticeWithTextCommonIc)`
+  width: 16.6rem;
+  height: 5.5rem;
+  margin-bottom: 2.8rem;
+  margin-left: -1.4rem;
+  flex-shrink: 0;
+`;
--- src/components/welcomeSignup/WelcomeSignup.tsx
+++ src/components/welcomeSignup/WelcomeSignup.tsx
@@ -1,3 +1,13 @@
+import { useState } from "react";
+import { styled } from "styled-components";
+import AfterSignup from "./AfterSignup";
+import AlertSignup from "./AlertSignup";
+
 export default function WelcomeSignup() {
-  return <div></div>;
+  const [isWelcome, setIsWelcome] = useState(true);
+  return <Container>{isWelcome ? <AfterSignup /> : <AlertSignup />}</Container>;
 }
+
+const Container = styled.div`
+  margin-left: 1.8rem;
+`;

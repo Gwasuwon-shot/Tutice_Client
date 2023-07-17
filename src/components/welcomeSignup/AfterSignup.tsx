95537dab81ff583f346c4e1d04b825baad92f7ca
Eunbeann <ds5bean23@gmail.com>
2023-07-17 19:39:44 +0900

    [ feat ] 메인 고정 문구 추가

--- src/components/welcomeSignup/AfterSignup.tsx
+++ src/components/welcomeSignup/AfterSignup.tsx
@@ -2,12 +2,18 @@ import { styled } from "styled-components";
 import { TuticeWithTextCommonIc } from "../../assets";
 
 export default function AfterSignup() {
+  const MAIN_TEXT = `이은수 선생님 환영합니다! \n 수업 나무 관리를 통해 \n 과외 관리를 시작해보세요. `;
   return (
-    <div>
+    <Container>
       <TuticeWithTextCommonIcon />
-    </div>
+      {MAIN_TEXT}
+    </Container>
   );
 }
+
+const Container = styled.div`
+  margin-top: 6.3rem;
+`;
 
 const TuticeWithTextCommonIcon = styled(TuticeWithTextCommonIc)`
   width: 10rem;
@@ -15,3 +21,5 @@ const TuticeWithTextCommonIcon = styled(TuticeWithTextCommonIc)`
   margin-bottom: 1.3rem;
   flex-shrink: 0;
 `;
+
+const MainText = styled.p``;

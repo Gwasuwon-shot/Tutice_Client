e659a656ce45173ae43825eb1b0232c20bbf4aa2
Seojisoo20191941 <amy0480@naver.com>
2023-07-18 05:46:10 +0900

    [ design ] 링크 공유 페이지 뷰 디자인

--- src/pages/LessonShare.tsx
+++ src/pages/LessonShare.tsx
@@ -10,23 +10,30 @@ export default function LessonShare() {
     navgiate("/");
   }
   return (
-    <>
+    <LessonShareWrapper>
       <LessonTreeSuccess>수업 나무 생성 완료!</LessonTreeSuccess>
-      <h1>수업나무의 링크를 학부모님께 공유해보세요</h1>
-      <p>학부모님에게 수업비와 출결에 관한 알림을 드릴 수 있어요</p>
+      <ShareTitle>
+        수업나무의 링크를 <br />
+        학부모님께 공유해보세요
+      </ShareTitle>
+      <SharSub>
+        학부모님에게 수업비와 출결에 관한 <br />
+        알림을 드릴 수 있어요
+      </SharSub>
 
-      <p>수업나무 링크</p>
-      <label>
+      <TreeTitle>수업나무 링크</TreeTitle>
+      <LinkBox>
         <CopylessonShareIc />
         <p>https://tuticetutice.com/kdfkdf11</p>
-      </label>
-      <ShareOthersLessonShareIc />
-      <KakaoLessonShareIc />
-
+      </LinkBox>
+      <ButtonWrapper>
+        <ShareOthersLessonShareIc />
+        <KakaoLessonShareIc />
+      </ButtonWrapper>
       <BottomButton isActive={true} onClick={handleMoveToHome} disabled={false} type="button">
         다음
       </BottomButton>
-    </>
+    </LessonShareWrapper>
   );
 }
 
@@ -35,4 +42,52 @@ const LessonTreeSuccess = styled.p`
 
   color: ${({ theme }) => theme.colors.green5};
   ${({ theme }) => theme.fonts.title02};
+`;
+
+const LessonShareWrapper = styled.section`
+  padding: 0 1.4rem;
+`;
+
+const ShareTitle = styled.h1`
+  margin-top: 0.6rem;
+  color: ${({ theme }) => theme.colors.grey900};
+  ${({ theme }) => theme.fonts.title01};
+`;
+
+const SharSub = styled.p`
+  margin-top: 1rem;
+  color: ${({ theme }) => theme.colors.grey500};
+  ${({ theme }) => theme.fonts.body06};
+`;
+
+const TreeTitle = styled.p`
+  margin-top: 2.5rem;
+  margin-bottom: 0.4rem;
+
+  color: ${({ theme }) => theme.colors.green4};
+  ${({ theme }) => theme.fonts.body06};
+`;
+
+const LinkBox = styled.label`
+  display: flex;
+  width: 29.2rem;
+  height: 4.6rem;
+  padding: 0.8rem;
+  justify-content: center;
+  align-items: center;
+  gap: 0.8rem;
+
+  border-radius: 8px;
+
+  background-color: ${({ theme }) => theme.colors.green1};
+
+  color: ${({ theme }) => theme.colors.grey700};
+  ${({ theme }) => theme.fonts.body02};
+`;
+
+const ButtonWrapper = styled.section`
+  display: flex;
+
+  justify-content: space-between;
+  margin-top: 6.6rem;
 `;

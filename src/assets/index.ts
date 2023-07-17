68d751fff7e81a5be681fd0f7b831a0998aa3e60
parkheeddong <phj7214go@gmail.com>
2023-07-17 17:54:27 +0900

    [ assets ] checkbox 아이콘

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -63,3 +63,5 @@ export { ReactComponent as UpcomingClassLogoTeacherHomeIc } from "./icon/upcomin
 export { ReactComponent as viewingLoginIc } from "./icon/viewingLoginIc.svg";
 export { ReactComponent as CancelButton } from "./icon/x_btn.svg";
 export { ReactComponent as RegularLessonGroupIc} from "./icon/regularLessonGroupIc.svg";
+export { ReactComponent as TuitionPaymentRadioButtonNotCheckedIc} from "./icon/tuitionPaymentRadioButtonNotCheckedIc.svg";
+export { ReactComponent as TuitionPaymentRadioButtonCheckedIc} from "./icon/tuitionPaymentRadioButtonCheckedIc.svg";

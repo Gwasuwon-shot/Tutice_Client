7a198099cc9eee4f14b368f23782db111a774ae1
pinktopaz <heather128@ewhain.net>
2023-07-19 03:06:23 +0900

    [ feat ] 푸시알림 리퀘스트 post 로직 추가

---
+++ src/api/postNotificationRequest.ts
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+export async function postNotificationRequest(token: string) {
+  const data = await axios.patch(
+    `${import.meta.env.VITE_APP_BASE_URL}/api/notifications`,
+    { deviceToken: token },
+    {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    },
+  );
+
+  console.log("postNotificationRequest" + data);
+  return data;
+}

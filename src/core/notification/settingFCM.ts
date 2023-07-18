77ca218955e06a78d600543dfb6047228c54dbb1
pinktopaz <heather128@ewhain.net>
2023-07-19 01:15:30 +0900

    [ etc ] env 값 가져오는 부분 수정

--- src/core/notification/settingFCM.ts
+++ src/core/notification/settingFCM.ts
@@ -9,7 +9,7 @@ import { getMessaging } from "firebase/messaging";
 export const firebaseConfig = {
   apiKey: import.meta.env.VITE_APP_FCM_API_KEY,
   authDomain: import.meta.env.VITE_APP_FCM_AUTH_DOMAIN,
-  projectId: "tutice-29eef",
+  projectId: import.meta.env.VITE_APP_FCM_PROJECT_ID,
   storageBucket: import.meta.env.VITE_APP_FCM_STORAGE_BUCKET,
   messagingSenderId: import.meta.env.VITE_APP_FCM_MESSAGING_SENDER_ID,
   appId: import.meta.env.VITE_APP_FCM_APP_ID,

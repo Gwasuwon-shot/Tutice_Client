d182bb508869a2250e2f8362580e7581ba03c2c9
pinktopaz <heather128@ewhain.net>
2023-07-19 02:38:33 +0900

    [ feat ] FCM이 서버에게 받은 알림내용을 클라에게 push해주기

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -6,3 +6,31 @@ self.addEventListener("install", function (e) {
 self.addEventListener("activate", function (e) {
   console.log("fcm sw activate..");
 });
+
+self.addEventListener("push", function (e) {
+  if (!e.data.json()) return;
+
+  const resultData = e.data.json().notification;
+  const notificationTitle = resultData.title;
+
+  const notificationOptions = {
+    body: resultData.body,
+  };
+
+  console.log(resultData.title, {
+    body: resultData.body,
+  });
+
+  e.waitUntil(
+    console.log("들어와라들어와라들어와라"),
+    self.registration.showNotification(notificationTitle, notificationOptions),
+    console.log("들어오냐고"),
+  );
+});
+
+// self.addEventListener("notificationclick", function (event) {
+//   console.log("notification click");
+//   const url = "/";
+//   event.notification.close();
+//   event.waitUntil(clients.openWindow(url));
+// });

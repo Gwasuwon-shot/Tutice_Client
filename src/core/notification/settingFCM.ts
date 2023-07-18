45f884f1f99ebd462ebdc80bb7bfdd5f63e4efbd
pinktopaz <heather128@ewhain.net>
2023-07-19 00:45:03 +0900

    [ etc ] fcm 세팅 추가

--- src/core/notification/settingFCM.ts
+++ src/core/notification/settingFCM.ts
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import { getMessaging } from "firebase/messaging";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 // Your web app's Firebase configuration
@@ -15,5 +16,6 @@ export const firebaseConfig = {
   measurementId: import.meta.env.VITE_APP_FCM_MEASUREMENT_ID,
 };
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+export const app = initializeApp(firebaseConfig);
+export const messaging = getMessaging(app);
 const analytics = getAnalytics(app);

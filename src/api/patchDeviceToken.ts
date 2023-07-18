67cc4549c29036078c7cf69b37024e5b74a20b32
pinktopaz <heather128@ewhain.net>
2023-07-19 01:40:26 +0900

    [ etc ] patchDeviceToken에 header 추가

--- src/api/patchDeviceToken.ts
+++ src/api/patchDeviceToken.ts
@@ -3,7 +3,15 @@ import { client } from "./axios";
 
 export async function patchDeviceToken(token: string) {
   console.log(token);
-  const data = await axios.patch(`${import.meta.env.VITE_APP_BASE_URL}/api/user/device-token`, { deviceToken: token });
+  const data = await axios.patch(
+    `${import.meta.env.VITE_APP_BASE_URL}/api/user/device-token`,
+    { deviceToken: token },
+    {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    },
+  );
 
   console.log(data);
   return data;

29ada6009f2feb7dd4699d7726251361a2916def
Seojisoo20191941 <amy0480@naver.com>
2023-07-19 00:41:43 +0900

    [ feat ] 선불 모달 구현

--- src/components/modal/PreypaymentModal.tsx
+++ src/components/modal/PreypaymentModal.tsx
@@ -1,18 +1,48 @@
+import { useNavigate } from "react-router-dom";
+import { styled } from "styled-components";
+import useModal from "../../hooks/useModal";
 import RoundBottomMiniButton from "../common/RoundBottomMiniButton";
+import ToastModal from "../common/ToastModal";
 
 export default function PreypaymentModal() {
+  const { unShowModal } = useModal();
+  const navigate = useNavigate();
+
+  function handleMoveToRegisterPayment() {
+    navigate("/register-payment/manageLessonId");
+  }
+
   return (
-    <>
-      <h1>선불 수업비를 받으셨나요?</h1>
-      <p>
-        수업비 입금 정보를 입력해주세요!
-        <RoundBottomMiniButton isGreen={false} onClick={""}>
+    <ToastModal>
+      <PrepaymentTitle>선불 수업비를 받으셨나요?</PrepaymentTitle>
+      <PrepaymentSub>수업비 입금 정보를 입력해주세요! </PrepaymentSub>
+      <ButtonWrapper>
+        <RoundBottomMiniButton isGreen={false} onClick={unShowModal}>
           다음에 입력할게요
         </RoundBottomMiniButton>
-        <RoundBottomMiniButton isGreen={true} onClick={""}>
+        <RoundBottomMiniButton isGreen={true} onClick={handleMoveToRegisterPayment}>
           입력하러 가기
         </RoundBottomMiniButton>
-      </p>
-    </>
+      </ButtonWrapper>
+    </ToastModal>
   );
 }
+
+const PrepaymentTitle = styled.h1`
+  margin-top: 5rem;
+  ${({ theme }) => theme.fonts.title02}
+`;
+
+const PrepaymentSub = styled.h2`
+  margin-top: 1.6rem;
+  ${({ theme }) => theme.fonts.body02}
+`;
+
+const ButtonWrapper = styled.section`
+  display: flex;
+  justify-content: space-between;
+
+  width: 29rem;
+
+  margin-top: 4rem;
+`;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,19 @@
 import { useState } from "react";
+import { useRecoilState } from "recoil";
+import { isModalOpen } from "../atom/common/isModalOpen";
+import PreypaymentModal from "../components/modal/PreypaymentModal";
 import ParentsHome from "../components/parentsHome/ParentsHome";
 import TeacherHome from "../components/teacherHome/TeacherHome";
 
 export default function Home() {
   const [isTeacherHome, setIsTeacherHome] = useState(true);
+  const [openModal, setOpenModal] = useRecoilState<boolean>(isModalOpen);
 
   return (
     <>
-      {/* 선생님인 경우 TeacherHome, 학부모인 경우 ParentHome 컴포넌트 띄워주기*/}
+      {/* idx가 있으면 선불 띄우기 */}
+      {openModal && <PreypaymentModal />}
+
       {isTeacherHome ? <TeacherHome /> : <ParentsHome />}
     </>
   );

b1c0c2084a1f366d4715dea7a703f0f27d6edaa3
pinktopaz <heather128@ewhain.net>
2023-07-18 19:46:10 +0900

    [ feat ] 디바이스 토큰 업데이트 함수 추가

---
+++ src/api/patchDeviceToken.ts
@@ -0,0 +1,9 @@
+import { client } from "./axios";
+
+export async function patchDeviceToken(deviceToken: string) {
+  console.log(deviceToken);
+  const data = await client.patch("/api/user/device-token", { deviceToken: deviceToken });
+
+  console.log(data);
+  return data;
+}

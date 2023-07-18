3e8b387116dce1c94c3490006f6933f61016cbf3
pinktopaz <heather128@ewhain.net>
2023-07-19 01:33:35 +0900

    [ etc ] axios 인스턴스 제거

--- src/api/patchDeviceToken.ts
+++ src/api/patchDeviceToken.ts
@@ -1,8 +1,9 @@
+import axios from "axios";
 import { client } from "./axios";
 
-export async function patchDeviceToken(deviceToken: string) {
-  console.log(deviceToken);
-  const data = await client.patch(`/api/user/device-token`, { deviceToken: deviceToken });
+export async function patchDeviceToken(token: string) {
+  console.log(token);
+  const data = await axios.patch(`${import.meta.env.VITE_APP_BASE_URL}/api/user/device-token`, { deviceToken: token });
 
   console.log(data);
   return data;

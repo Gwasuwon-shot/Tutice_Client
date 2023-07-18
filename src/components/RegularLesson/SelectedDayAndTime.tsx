08856ea9fb0ef2f1944eb46cb08cc00a50006694
parkheeddong <phj7214go@gmail.com>
2023-07-18 12:47:34 +0900

    [ feat ] 수업일시블록 props전달 로직

--- src/components/RegularLesson/SelectedDayAndTime.tsx
+++ src/components/RegularLesson/SelectedDayAndTime.tsx
@@ -3,12 +3,22 @@ import {RegularLessonGroupIc} from "../../assets";
 import styled from 'styled-components';
 import {useRecoilValue} from 'recoil';
 
-export default function SelectedDayAndTime() {
+interface selectedProps {
+    dayofweek : string;
+    startTime : string;
+    endTime : string;
+}
 
+export default function SelectedDayAndTime(props : selectedProps) {
+    // dayofweek = {'월'} startTime = {'1시'} endTime = {'2시'} 
+
+    const { dayofweek, startTime, endTime } = props;
 
     return (
         <SelectedWrapper>
             <RegularLessonGroupIcon />
+            <DayWrapper> {dayofweek} </DayWrapper>
+            <TimeWrapper> {startTime} - {endTime} </TimeWrapper>
         </SelectedWrapper>
     );
 }
@@ -20,7 +30,7 @@ const SelectedWrapper = styled.article`
     width: 29.2rem;
     height: 3.6rem;
     padding: 1rem;
-    margin: 1.4rem;
+    margin: 1.5rem;
 
     border-radius: 7px;
     background-color: ${({ theme }) => theme.colors.green1}; 
@@ -32,15 +42,18 @@ const RegularLessonGroupIcon = styled(RegularLessonGroupIc)`
     height: 1.8rem;
 `
 
-const DayWrapepr = styled.div`
-    display: flex;
-    width: 13rem;
-    ${({ theme }) => theme.font.body03}; 
-    color: ${({ theme }) => theme.colors.green5}; 
-`
-const TimeWrapper = styled.div`
+const DayWrapper = styled.div`
     display: flex;
     width: 12rem;
-    ${({ theme }) => theme.font.body04}; 
+    margin-left: 1.8rem;
+    ${({ theme }) => theme.fonts.body03}; 
+    color: ${({ theme }) => theme.colors.green5}; 
+`
+
+const TimeWrapper = styled.div`
+    display: flex;
+    justfiy-content: flex-end;
+    width: 12rem;
+    ${({ theme }) => theme.fonts.body04}; 
     color: ${({ theme }) => theme.colors.green5}; 
 `

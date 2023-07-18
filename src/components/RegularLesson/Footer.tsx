e9b36aeea30f4683dc2a27cc443e60050070182c
parkheeddong <phj7214go@gmail.com>
2023-07-18 21:18:49 +0900

    [ feat ] 저장버튼 활성화 로직 구현

--- src/components/RegularLesson/Footer.tsx
+++ src/components/RegularLesson/Footer.tsx
@@ -1,5 +1,6 @@
-import { dayState, focusDayState } from "../../atom/timePicker/timePicker";
+import { dayState, firstLessonDay, focusDayState } from "../../atom/timePicker/timePicker";
 import { openDatePickerState, openFinishDetailState, openStartDetailState, openTimePickerState } from "../../atom/timePicker/timePicker";
+import {useEffect, useState} from 'react';
 
 import DatePicker from '../../components/RegularLesson/TimePicker/DatePicker';
 import DetailTimePicker from '../../components/RegularLesson/TimePicker/DetailTimePicker';
@@ -15,15 +16,32 @@ export default function Footer() {
     const [isStartPickerOpen, setIsStartPickerOpen] = useRecoilState<boolean>(openStartDetailState);
     const [isFinishPickerOpen, setIsFinishPickerOpen] = useRecoilState<boolean>(openFinishDetailState);
 
+    const [firstday, setFirstday] = useRecoilState(firstLessonDay);
+    const [selectedDays, setSelectedDays] = useRecoilState(dayState);
+
+    let [isSame, setIsSame] = useState(false);
+    
+    // 저장버튼 활성화 로직 : 수업일시 확정요일 배열과 첫 수업일이 변경될 경우, 배열을 순회하여 해당 수업일이 존재하는지 체크
+    useEffect(() => {
+        setIsSame(false);
+        for (let idx in selectedDays) {
+            if (selectedDays[idx].dayOfWeek === firstday) {
+                setIsSame(true);
+                break;
+            }
+        }
+    }, [selectedDays, firstday]);
+    
+    function CompleteLesson() {
     
 
-    function CompleteLesson() {
-        
+        // 첫 수업일과 정기등록 요일은 동일해야 해요 
     }
+    
     return (
         <>
-        <FooterWrapper onClick = {CompleteLesson}> 
-            <FooterButton> 저장 </FooterButton>
+        <FooterWrapper onClick = {CompleteLesson} selected = {isSame}> 
+            <FooterButton selected = {isSame}> 저장 </FooterButton>
         </FooterWrapper>
         {isTimePickerOpen && <ModalWrapper> <TimePicker /> </ModalWrapper>}
         {isDatePickerOpen && <ModalWrapper> <DatePicker /> </ModalWrapper>}
@@ -32,7 +50,7 @@ export default function Footer() {
     );
 }
 
-const FooterWrapper = styled.footer`
+const FooterWrapper = styled.footer<{selected: boolean}>`
     display: flex;
     flex-direction: column;
     align-items: center;
@@ -45,13 +63,15 @@ const FooterWrapper = styled.footer`
     padding: 0.8rem;
     
     background-color: ${({ theme }) => theme.colors.grey50}; 
+    ${({ selected, theme }) => selected && `background-color: ${theme.colors.green5};`}
 `
 
-const FooterButton = styled.button`
+const FooterButton = styled.button<{selected: boolean}>`
     display: flex;
     
     ${({ theme }) => theme.fonts.body02};
     color: ${({ theme }) => theme.colors.grey200}; 
+    ${({ selected, theme }) => selected && `color: ${theme.colors.white};`}
 `
 
 const ModalWrapper = styled.div`

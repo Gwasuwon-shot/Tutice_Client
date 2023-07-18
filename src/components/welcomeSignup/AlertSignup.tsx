6af58976e55aa8a47f3094928b2a61383960ae1e
pinktopaz <heather128@ewhain.net>
2023-07-18 20:43:14 +0900

    [ feat ] onClick시 서비스 워커 등록하기

--- src/components/welcomeSignup/AlertSignup.tsx
+++ src/components/welcomeSignup/AlertSignup.tsx
@@ -2,6 +2,7 @@ import { styled } from "styled-components";
 import { bellWelcomeIc } from "../../assets";
 import SignupTitleLayout from "../signup/SignupTitleLayout";
 import ButtonLayout from "./ButtonLayout";
+import { registerServiceWorker } from "../../utils/common/notification";
 
 export default function AlertSignup() {
   const MAIN_TEXT = `수업 나무를 통한 \n 쉬운 관리를 위해\n 알림을 활성화 해보세요 `;
@@ -20,6 +21,8 @@ export default function AlertSignup() {
     }
   }
 
+  registerServiceWorker();
+
   return (
     <>
       <Container>
@@ -28,7 +31,7 @@ export default function AlertSignup() {
         <SubText>{SUB_TEXT}</SubText>
       </Container>
 
-      <ButtonLayout onClick={handleAllowNotification} buttonText={"할래요!"} />
+      <ButtonLayout onClick={() => handleAllowNotification()} buttonText={"할래요!"} />
     </>
   );
 }

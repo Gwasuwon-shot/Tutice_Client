b3d985fa2e63e2f7371014045d604aaa752f4206
parkheeddong <phj7214go@gmail.com>
2023-07-19 17:42:43 +0900

    [ style ] 블록 많이 생성 시 스크롤되도록 설정

--- src/components/RegularLesson/Footer.tsx
+++ src/components/RegularLesson/Footer.tsx
@@ -37,29 +37,33 @@ export default function Footer() {
     }
     
     return (
-        <>
-        <FooterWrapper onClick = {CompleteLesson} selected = {isSame}> 
-            <FooterButton disabled = {isSame}> 저장 </FooterButton>
+        <FooterWrapper>
+            <FooterButtonWrapper onClick = {CompleteLesson} selected = {isSame}> 
+                <FooterButton disabled = {isSame}> 저장 </FooterButton>
+            </FooterButtonWrapper>
+            {isTimePickerOpen && <ModalWrapper> <TimePicker /> </ModalWrapper>}
+            {isDatePickerOpen && <ModalWrapper> <DatePicker /> </ModalWrapper>}
+            {(isStartPickerOpen || isFinishPickerOpen) && <ModalWrapper> <DetailTimePicker /> </ModalWrapper>}
         </FooterWrapper>
-        {isTimePickerOpen && <ModalWrapper> <TimePicker /> </ModalWrapper>}
-        {isDatePickerOpen && <ModalWrapper> <DatePicker /> </ModalWrapper>}
-        {(isStartPickerOpen || isFinishPickerOpen) && <ModalWrapper> <DetailTimePicker /> </ModalWrapper>}
-        </>
     );
 }
 
-const FooterWrapper = styled.footer<{selected: boolean}>`
+const FooterWrapper = styled.div`
+    height: 7rem;
+`
+
+const FooterButtonWrapper = styled.footer<{selected: boolean}>`
     display: flex;
     flex-direction: column;
     align-items: center;
-    
+
     position: fixed;
     bottom: 0;
-    
+
     width: 100%;
     height: 6.3rem;
     padding: 0.8rem;
-    
+
     background-color: ${({ theme }) => theme.colors.grey50}; 
     ${({ selected, theme }) => selected && `background-color: ${theme.colors.green5};`}
 `

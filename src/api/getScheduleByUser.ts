33374b12ece9f9035a0945612a8758ad87b4af52
HAE2NI <hae0_02ni@ewhain.net>
2023-07-19 17:07:48 +0900

    [ fix ] 오류 수정 (오타)

--- src/api/getScheduleByUser.ts
+++ src/api/getScheduleByUser.ts
@@ -2,10 +2,11 @@ import axios from "axios";
 import React from "react";
 
 export async function getScheduleByUser(date: string) {
+  console.log(import.meta.env.VITE_APP_TEACHER_TOKEN);
   const data = await axios.get(`${import.meta.env.VITE_APP_BASE_URL}/api/schedule/?month=${date}`, {
     headers: {
       "Content-Type": "application/json",
-      Authorization: `Bearer ${import.meta.env.VITE_APP_PARENTS_TOKEN}`,
+      Authorization: `Bearer ${import.meta.env.VITE_APP_TEACHER_TOCKEN}`,
     },
   });
   console.log(data);

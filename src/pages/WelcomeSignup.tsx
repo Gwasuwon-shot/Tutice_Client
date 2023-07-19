c009fefa06521bdb05f2fd06cb1ef9123e0660c7
Eunbeann <ds5bean23@gmail.com>
2023-07-20 01:53:26 +0900

    [ feat ] 라우팅 연결

---
+++ src/pages/WelcomeSignup.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { styled } from "styled-components";
+
+export default function WelcomeSignup() {
+  return (
+    <Container>
+      <WelcomeSignup />
+    </Container>
+  );
+}
+
+const Container = styled.div`
+  margin-left: 1.4rem;
+`;

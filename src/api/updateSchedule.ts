ec4854209a2861cb20b64c1fa752808789d8900a
HAE2NI <hae0_02ni@ewhain.net>
2023-07-19 22:14:45 +0900

    [ feat ] updateSchedule api 함수 구현

--- src/api/updateSchedule.ts
+++ src/api/updateSchedule.ts
@@ -1,12 +1,32 @@
 import axios from "axios";
 import React from "react";
 
-export async function updateSchedule() {
-  const data = await axios.get(`${import.meta.env.VITE_APP_BASE_URL}/api/schedule`, {
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: `Bearer ${import.meta.env.VITE_APP_TEACHER_TOCKEN}`,
+interface updateScheduleType {
+  idx: number;
+  date: string;
+  startTime: string;
+  endTime: string;
+}
+
+export async function updateSchedule(props: updateScheduleType) {
+  const { idx, date, startTime, endTime } = props;
+  const data = await axios.patch(
+    `${import.meta.env.VITE_APP_BASE_URL}/api/schedule`,
+    {
+      schedule: {
+        idx: idx,
+        date: date,
+        startTime: startTime,
+        endTime: endTime,
+      },
     },
-  });
+    {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${import.meta.env.VITE_APP_TEACHER_TOCKEN}`,
+      },
+    },
+  );
   console.log(data);
+  return data;
 }

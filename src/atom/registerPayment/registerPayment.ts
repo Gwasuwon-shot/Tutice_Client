aa645ee11236abb0f3ff8cab1b7e52bb40a48189
Seojisoo20191941 <amy0480@naver.com>
2023-07-20 17:17:38 +0900

    [ fix ] 리코일 유니크 키 아닌 것 수정

--- src/atom/registerPayment/registerPayment.ts
+++ src/atom/registerPayment/registerPayment.ts
@@ -1,11 +1,11 @@
-import { atom } from 'recoil';
+import { atom } from "recoil";
 
 export const openPaymentPicker = atom<boolean>({
-    key: 'openPaymentPicker',
-    default: false,
-})
+  key: "openPaymentPicker",
+  default: false,
+});
 
 export const paymentDateState = atom({
-    key: 'dateState',
-    default: {year : new Date().getFullYear(), month : new Date().getMonth()+1, date: new Date().getDate()}
+  key: "paymentDateState",
+  default: { year: new Date().getFullYear(), month: new Date().getMonth() + 1, date: new Date().getDate() },
 });

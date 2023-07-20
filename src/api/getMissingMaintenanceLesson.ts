3e20f1082903f4c1ce033f0bdf9928becf5b982a
parkheeddong <phj7214go@gmail.com>
2023-07-21 02:32:17 +0900

    [ feat ] 연장여부 알리지 않은 수업목록 api함수생성

---
+++ src/api/getMissingMaintenanceLesson.ts
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+export async function getMissingMaintenanceLesson() {
+  const data = await axios.get(`${import.meta.env.VITE_APP_BASE_URL}/api/lesson/maintenance/missing`, {
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${import.meta.env.VITE_APP_TEACHER_TOCKEN}`,
+    },
+  });
+
+  return data.data.data.getMissingMaintenanceLesson;
+}

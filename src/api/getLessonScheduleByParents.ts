e6deb754706264de9e7844e49443ffc9b0eb77c8
HAE2NI <hae0_02ni@ewhain.net>
2023-07-20 20:01:22 +0900

    [ etc ] 쓸모없는 코드 삭제

--- src/api/getLessonScheduleByParents.ts
+++ src/api/getLessonScheduleByParents.ts
@@ -1,7 +1,6 @@
 import axios from "axios";
 
 export async function getLessonScheduleByParents(lessonIdx: number) {
-  console.log(lessonIdx);
   const data = await axios.get(`${import.meta.env.VITE_APP_BASE_URL}/api/lesson/schedule/parents/${lessonIdx}`, {
     headers: {
       "Content-Type": "application/json",

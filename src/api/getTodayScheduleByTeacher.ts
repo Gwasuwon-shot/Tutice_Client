cca01db953a5941d6033ae7ecefa70497d215dcf
Seojisoo20191941 <amy0480@naver.com>
2023-07-20 16:14:27 +0900

    [ feat ] 배너 패칭

---
+++ src/api/getTodayScheduleByTeacher.ts
@@ -0,0 +1,14 @@
+import axios from "axios";
+
+export async function getTodayScheduleByTeacher() {
+  const data = await axios.get(`${import.meta.env.VITE_APP_BASE_URL}/api/schedule/today/teacher`, {
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${import.meta.env.VITE_APP_TEACHER_TOCKEN}`,
+    },
+  });
+
+  console.log(data);
+
+  return data.data.data;
+}
--- src/hooks/useGetTodayScheduleByTeacher.ts
+++ src/hooks/useGetTodayScheduleByTeacher.ts
@@ -1,9 +1,19 @@
-import { YES_TODAY_CLASS_BEFORE_CLASS_BANNER } from "../core/teacherHome/teacherHome";
+import { useQuery } from "react-query";
+import { getTodayScheduleByTeacher } from "../api/getTodayScheduleByTeacher";
 
 export default function useGetTodayScheduleByTeacher() {
   // 선생님 : 메인페이지 뷰 (홈)- 배너(가장 가까운 수업)
   //   api 패칭
-  const { teacherName, isTodaySchedule, todaySchedule } = YES_TODAY_CLASS_BEFORE_CLASS_BANNER?.data;
+  const { data: todayScheduleByTeacher } = useQuery(["todayScheduleByTeacher"], getTodayScheduleByTeacher, {
+    onSuccess: () => {},
+    onError: (error) => {
+      console.log(error);
+    },
+  });
+
+  const teacherName = todayScheduleByTeacher?.teacherName;
+  const isTodaySchedule = todayScheduleByTeacher?.isTodaySchedule;
+  const todaySchedule = todayScheduleByTeacher?.todaySchedule;
 
   return { teacherName, isTodaySchedule, todaySchedule };
 }

dc6773571bdb017a5e38a346bec31d2e1d6a3b52
Seojisoo20191941 <amy0480@naver.com>
2023-07-21 19:31:14 +0900

    [ etc ] 불필요 파일 삭제

--- src/hooks/useGetPaymentRecord.ts
+++
@@ -1,8 +0,0 @@
-import { GET_PAYMENT_RECORD } from "../core/manageLesson/getPaymentRecord";
-
-export default function useGetPaymentRecord() {
-  //   api 패칭
-  const { lesson, paymentDate } = GET_PAYMENT_RECORD?.data;
-
-  return { lesson, paymentDate };
-}

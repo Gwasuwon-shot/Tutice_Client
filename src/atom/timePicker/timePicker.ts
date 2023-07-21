fbdb29cdd3796c549302af082519020bd11c5021
HAE2NI <hae0_02ni@ewhain.net>
2023-07-21 22:36:45 +0900

    [ etc ] persistAtom 추가

--- src/atom/timePicker/timePicker.ts
+++ src/atom/timePicker/timePicker.ts
@@ -1,37 +1,39 @@
-import { atom } from 'recoil';
+import { atom } from "recoil";
+import { recoilPersist } from "recoil-persist";
+
+const { persistAtom } = recoilPersist();
 
 // 모달 오픈여부 관리
 export const openTimePickerState = atom<boolean>({
-    key: 'openTimePickerState',
-    default: false,
-})
+  key: "openTimePickerState",
+  default: false,
+});
 
 export const openDatePickerState = atom<boolean>({
-    key: 'openDatePickerState',
-    default: false,
-})
+  key: "openDatePickerState",
+  default: false,
+});
 
 export const openStartDetailState = atom<boolean>({
-  key: 'openStartDetailState',
+  key: "openStartDetailState",
   default: false,
-})
+});
 
 export const openFinishDetailState = atom<boolean>({
-  key: 'openFinishDetailState',
+  key: "openFinishDetailState",
   default: false,
-})
-
+});
 
 // 회차 관리
 export const cycleNumberState = atom<number>({
-  key: 'cycleNumberState',
+  key: "cycleNumberState",
   default: -1,
-})
+});
 
 // 첫 수업일 관리
 export const dateState = atom({
-  key: 'dateState',
-  default: {year : new Date().getFullYear(), month : new Date().getMonth()+1, date: new Date().getDate()}
+  key: "dateState",
+  default: { year: new Date().getFullYear(), month: new Date().getMonth() + 1, date: new Date().getDate() },
 });
 
 interface Day {
@@ -42,24 +44,25 @@ interface Day {
 
 // 확정 날짜들 관리
 export const dayState = atom<Day[]>({
-  key: 'dayState',
+  key: "dayState",
   default: [],
 });
 
 // 현재 선택중인 날짜 관리
 export const focusDayState = atom({
-  key: 'focusDayState',
-  default: {dayOfWeek: ['일','월','화','수','목','금','토'][new Date().getDay()], startTime: '', endTime: ''},
-})
+  key: "focusDayState",
+  default: { dayOfWeek: ["일", "월", "화", "수", "목", "금", "토"][new Date().getDay()], startTime: "", endTime: "" },
+});
 
 // 확정날짜 '저장' 시, 선택한 첫 수업일의 요일을 푸터에서 확인하기 위한 atom
 export const firstLessonDay = atom({
-  key: 'firstLessonDay',
-  default: {1: '월', 2: '화', 3: '수', 4: '목', 5: '금', 6: '토', 0: '일'}[new Date().getDay()],
-})
+  key: "firstLessonDay",
+  default: { 1: "월", 2: "화", 3: "수", 4: "목", 5: "금", 6: "토", 0: "일" }[new Date().getDay()],
+});
 
 // 임시 날짜 저장 ('캘린더로 일정 확인')
 export const temporarySchedule = atom({
-  key: 'temporarySchedule',
-  default: {}
-})
+  key: "temporarySchedule",
+  default: {},
+  effects_UNSTABLE: [persistAtom],
+});

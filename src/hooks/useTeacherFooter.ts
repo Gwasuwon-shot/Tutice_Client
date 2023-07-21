bfb0e3a962be87b604aa727bbc1be38a10472cb1
Seojisoo20191941 <amy0480@naver.com>
2023-07-21 16:38:21 +0900

    [ style ] 푸터 내용 컴포넌트와 커스텀 훅으로 분리

---
+++ src/hooks/useTeacherFooter.ts
@@ -0,0 +1,51 @@
+import { useNavigate } from "react-router-dom";
+import { useRecoilState } from "recoil";
+import { teacherFooterCategory } from "../atom/teacherFooterCategory";
+import { TEACHER_FOOTER_CATEGORY } from "../core/teacherHome/teacherFooter";
+import { TeacherFooterType } from "../type/teacherHome/teacherFooterType";
+
+export default function useTeacherFooter() {
+  const [teacherFooterList, setTeacherFooterList] = useRecoilState<TeacherFooterType[]>(teacherFooterCategory);
+  const navigate = useNavigate();
+
+  // function showTeacherFooterIcon(category: string, isMoved: boolean) {
+  //   switch (category) {
+  //     case TEACHER_FOOTER_CATEGORY.home:
+  //       return isMoved ? <HomeActiveTeacherFooterIcon /> : <HomeTeacherFooterIcon />;
+  //     case TEACHER_FOOTER_CATEGORY.calendar:
+  //       return isMoved ? <CalendarActiveTeacherFooterIcon /> : <CalendarTeacherFooterIcon />;
+  //     case TEACHER_FOOTER_CATEGORY.classManaging:
+  //       return isMoved ? <ClassManagingActiveTeacherFooterIcon /> : <ClassManagingTeacherFooterIcon />;
+  //     case TEACHER_FOOTER_CATEGORY.my:
+  //       return isMoved ? <MyActiveTeacherFooterIcon /> : <MyTeacherFooterIcon />;
+  //     default:
+  //       return;
+  //   }
+  // }
+
+  function handleMoveToPage(category: string) {
+    setTeacherFooterList(
+      teacherFooterList.map((list) =>
+        list.category === category ? { ...list, isMoved: true } : { ...list, isMoved: false },
+      ),
+    );
+    switch (category) {
+      case TEACHER_FOOTER_CATEGORY.home:
+        navigate("/");
+        break;
+      case TEACHER_FOOTER_CATEGORY.calendar:
+        navigate("/schedule");
+        break;
+      case TEACHER_FOOTER_CATEGORY.classManaging:
+        navigate("/manage-lesson");
+        break;
+      case TEACHER_FOOTER_CATEGORY.my:
+        navigate("/mypage");
+        break;
+      default:
+        break;
+    }
+  }
+
+  return { handleMoveToPage };
+}

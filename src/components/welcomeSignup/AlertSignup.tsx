759b2cd3457344c8c9447c568de6878a437a1572
Seojisoo20191941 <amy0480@naver.com>
2023-07-22 04:23:10 +0900

    [ etc ] 수정 중

--- src/components/welcomeSignup/AlertSignup.tsx
+++ src/components/welcomeSignup/AlertSignup.tsx
@@ -33,6 +33,7 @@ export default function AlertSignup(prop: AlertSignupProp) {
   async function handleAllowNotification() {
     const permission = await Notification.requestPermission();
 
+    console.log(permission);
     registerServiceWorker();
 
     try {

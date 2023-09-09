37c576293472fe63ea7c6bc7a83e0a6b8bfbc729
parkheeddong <phj7214go@gmail.com>
2023-09-09 19:40:54 +0900

    [ fix ] 등록한 수업 블록 변경

--- src/components/RegularLesson/SelectedDayAndTime.tsx
+++ src/components/RegularLesson/SelectedDayAndTime.tsx
@@ -16,38 +16,49 @@ export default function SelectedDayAndTime(props : selectedProps) {
     const { dayofweek, startTime, endTime } = props;
     const [selectedDays, setSelectedDays] = useRecoilState(dayState);
 
-    function handleDeleteDay() {
-        const newList = selectedDays.filter(param => param.dayOfWeek !== dayofweek)
-        setSelectedDays(newList);
-    };
+    // 블록 삭제
+    // function handleDeleteDay() {
+    //     const newList = selectedDays.filter(param => param.dayOfWeek !== dayofweek)
+    //     setSelectedDays(newList);
+    // };
 
     return (
         <SelectedWrapper>
-            <RegularLessonGroupIcon onClick = {handleDeleteDay}/>
-            <DayWrapper> {dayofweek} </DayWrapper>
+            <DayWrapper> {dayofweek}요일 </DayWrapper>
             <TimeWrapper> 
-                {Number(startTime.slice(0, 2)) <= 12 ? (
-                    <> 오전 {Number(startTime.slice(0, 2))}시 {startTime.slice(3)}분 </>
-                    ) : (
-                    <> 오후 {Number(startTime.slice(0, 2)) - 12}시 {startTime.slice(3)}분 </>
-                )}  - 
-                {Number(endTime.slice(0, 2)) <= 12 ? (
-                    <> 오전 {Number(endTime.slice(0, 2))}시 {endTime.slice(3)}분 </>
-                    ) : (
-                    <> 오후 {Number(endTime.slice(0, 2)) - 12}시 {endTime.slice(3)}분 </>
-                )}
-            </TimeWrapper>
+                <Time>
+                    <TimeInfo> 시작 </TimeInfo>
+                    <DetailTime>
+                        {Number(startTime.slice(0, 2)) <= 12 ? (
+                        <> 오전 {Number(startTime.slice(0, 2))}시 {startTime.slice(3)}분 </>
+                        ) : (
+                        <> 오후 {Number(startTime.slice(0, 2)) - 12}시 {startTime.slice(3)}분 </>
+                        )}
+                    </DetailTime>
+                </Time>
+                <Time>
+                    <TimeInfo> 종료 </TimeInfo>
+                    <DetailTime>
+                        {Number(endTime.slice(0, 2)) <= 12 ? (
+                        <> 오전 {Number(endTime.slice(0, 2))}시 {endTime.slice(3)}분 </>
+                        ) : (
+                        <> 오후 {Number(endTime.slice(0, 2)) - 12}시 {endTime.slice(3)}분 </>
+                        )}
+                    </DetailTime>
+                </Time>
+            </TimeWrapper>            
         </SelectedWrapper>
     );
 }
 
 const SelectedWrapper = styled.article`
     display: flex;
-    align-items: center;
-
+    flex-direction: column;
+    justify-content: center;
+    gap: 0.8rem;
     width: 29.2rem;
-    height: 3.6rem;
-    padding: 1rem;
+    height: 8rem;
+    padding: 0.9rem;
     margin-left: 1.5rem;
     margin-right: 1.5rem;
     margin-bottom: 0.4rem;
@@ -57,14 +68,8 @@ const SelectedWrapper = styled.article`
     color: ${({ theme }) => theme.colors.green5}; 
 `
 
-const RegularLessonGroupIcon = styled(RegularLessonGroupIc)`
-    width: 1.8rem;
-    height: 1.8rem;
-`
-
 const DayWrapper = styled.div`
     display: flex;
-    margin-left: 1.8rem;
     width: 5rem;
     ${({ theme }) => theme.fonts.body03}; 
     color: ${({ theme }) => theme.colors.green5}; 
@@ -72,8 +77,28 @@ const DayWrapper = styled.div`
 
 const TimeWrapper = styled.div`
     display: flex;
-    justify-content: end;
-    width: 20rem;
+    gap: 1rem;
+    width: 28rem;
+`
+
+const Time = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 0.7rem;
+    width: 13rem;
+    height: 3.4rem;
+    background-color: ${({ theme }) => theme.colors.green2}; 
+    border-radius: 7px;
+`
+
+const TimeInfo = styled.p`
+    ${({ theme }) => theme.fonts.body04}; 
+    color: ${({ theme }) => theme.colors.grey0}; 
+`
+
+const DetailTime = styled.p`
     ${({ theme }) => theme.fonts.body04}; 
     color: ${({ theme }) => theme.colors.green5}; 
 `
+

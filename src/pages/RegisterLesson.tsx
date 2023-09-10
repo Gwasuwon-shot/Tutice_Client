d1c135e021b2e0203fa0da665a5669c141f706ef
Seojisoo20191941 <amy0480@naver.com>
2023-09-11 00:40:30 +0900

    [ feat ] 선불 후불 값 초기화

--- src/pages/RegisterLesson.tsx
+++ src/pages/RegisterLesson.tsx
@@ -1,13 +1,22 @@
+import { useEffect } from "react";
+import { useRecoilState } from "recoil";
+import { paymentOrder } from "../atom/tuitionPayment/tuitionPayment";
 import Footer from "../components/RegisterLessonPage/Footer";
 import Header from "../components/RegisterLessonPage/Header";
 import LessonInput from "../components/RegisterLessonPage/LessonInput";
 
 export default function RegisterLesson() {
+  const [payment, setPayment] = useRecoilState(paymentOrder);
+
+  useEffect(() => {
+    setPayment("");
+  }, []);
+
   return (
-  <>
-    <Header />
-    <LessonInput />
-    <Footer />
-  </>
+    <>
+      <Header />
+      <LessonInput />
+      <Footer />
+    </>
   );
 }

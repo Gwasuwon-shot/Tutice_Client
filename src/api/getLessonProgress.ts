43aba39b557ceaecff8c169161411606bbf72a68
pinktopaz <heather128@ewhain.net>
2023-09-10 22:41:58 +0900

    [ refactor ] 수업 현황 api 수정 반영

--- src/api/getLessonProgress.ts
+++ src/api/getLessonProgress.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { getCookie } from "./cookie";
 
 export async function getLessonProgress(lessonIdx: number) {
-  const data = await axios.get(`${import.meta.env.VITE_APP_BASE_URL}/api/lesson/progress/${lessonIdx}`, {
+  const data = await axios.get(`${import.meta.env.VITE_APP_BASE_URL}/api/lesson/${lessonIdx}/progress`, {
     headers: {
       "Content-Type": "application/json",
       Authorization: `Bearer ${getCookie("accessToken")}`,

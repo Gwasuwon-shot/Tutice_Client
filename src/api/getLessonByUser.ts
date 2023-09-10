9703703e33e2d9efc9e65e811439c134d63d63f8
Seojisoo20191941 <amy0480@naver.com>
2023-09-10 21:50:01 +0900

    [ fix ] api url 변경 사항 반영

--- src/api/getLessonByUser.ts
+++ src/api/getLessonByUser.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { getCookie } from "./cookie";
 
 export async function getLessonByUser() {
-  const data = await axios.get(`${import.meta.env.VITE_APP_BASE_URL}/api/lesson`, {
+  const data = await axios.get(`${import.meta.env.VITE_APP_BASE_URL}/api/lesson/existence`, {
     headers: {
       "Content-Type": "application/json",
       Authorization: `Bearer ${getCookie("accessToken")}`,
--- src/api/getMissingAttendanceSchedule.ts
+++ src/api/getMissingAttendanceSchedule.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { getCookie } from "./cookie";
 
 export async function getMissingAttendanceSchedule() {
-  const data = await axios.get(`${import.meta.env.VITE_APP_BASE_URL}/api/schedule/attendance/missing`, {
+  const data = await axios.get(`${import.meta.env.VITE_APP_BASE_URL}/api/schedule/missing-attendance`, {
     headers: {
       "Content-Type": "application/json",
       Authorization: `Bearer ${getCookie("accessToken")}`,

3857803ca6e9e28b74f1b58060e89ba20da0cd16
Seojisoo20191941 <amy0480@naver.com>
2023-09-11 15:00:28 +0900

    [ feat ] 선생님, 학부모 로직 추가

--- src/components/welcomeSignup/WelcomeLayout.tsx
+++ src/components/welcomeSignup/WelcomeLayout.tsx
@@ -1,18 +1,24 @@
 import { useState } from "react";
-import { useSetRecoilState } from "recoil";
+import { useRecoilValue, useSetRecoilState } from "recoil";
 import { styled } from "styled-components";
+import { userRoleData } from "../../atom/loginUser/loginUser";
 import { stepNum } from "../../atom/signup/signup";
 import AfterSignup from "./AfterSignup";
 import AlertSignup from "./AlertSignup";
 
 export default function WelcomeLayout() {
+  const userRole = useRecoilValue(userRoleData);
   const [isWelcome, setIsWelcome] = useState<boolean>(true);
   const setStep = useSetRecoilState(stepNum);
 
   return (
     <>
       <Container>
-        {isWelcome ? <AfterSignup setIsWelcome={setIsWelcome} /> : <AlertSignup setIsWelcome={setIsWelcome} />}
+        {userRole === "선생님" ? (
+          <AfterSignup setIsWelcome={setIsWelcome} />
+        ) : (
+          <AlertSignup setIsWelcome={setIsWelcome} />
+        )}
       </Container>
     </>
   );

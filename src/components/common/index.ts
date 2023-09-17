8b1b1f4247eaeffc20d66ad5aacb9f03b3e2a8f8
eunbeann <ds5bean23@gmail.com>
2023-09-17 19:44:32 +0900

    [ feat ] common 컴포넌트 index.ts 정리

---
+++ src/components/common/index.ts
@@ -0,0 +1,30 @@
+// index.ts 파일
+
+export { default as TreeProgress } from "./TreeProgress";
+export { default as AttendanceCheckButton } from "./AttendanceCheckButton";
+export { default as AttendanceCheckModal } from "./AttendanceCheckModal";
+export { default as AttendanceDoubleCheckingModal } from "./AttendanceDoubleCheckingModal";
+export { default as AttendanceStatusButton } from "./AttendanceStatusButton";
+export { default as BackButton } from "./BackButton";
+export { default as BasicDoubleModal } from "./BasicDoubleModal";
+export { default as BasicSingleModal } from "./BasicSingleModal";
+export { default as BottomButton } from "./BottomButton";
+export { default as CancelLessonMaintenanceSnackBar } from "./CancelLessonMaintenanceSnackBar";
+export { default as CommonBackButton } from "./CommonBackButton";
+export { default as CreateTreeCode } from "./CreateTreeCode";
+export { default as Header } from "./Header";
+export { default as NoCheckPageAttendanceButton } from "./NoCheckPageAttendanceButton";
+export { default as ParentsFooter } from "./ParentsFooter";
+export { default as ParentsFooterIcons } from "./ParentsFooterIcons";
+export { default as ProgressBar } from "./ProgressBar";
+export { default as RoundBottomButton } from "./RoundBottomButton";
+export { default as RoundBottomMiniButton } from "./RoundBottomMiniButton";
+export { default as SendAlarmCheckModal } from "./SendAlarmCheckModal";
+export { default as SnackBarPopup } from "./SnackBarPopup";
+export { default as StudentColorBox } from "./StudentColorBox";
+export { default as StudentNameLabel } from "./StudentNameLabel";
+export { default as SubjectLabel } from "./SubjectLabel";
+export { default as SuccessSendingAlarmSnackBar } from "./SuccessSendingAlarmSnackBar";
+export { default as TeacherFooter } from "./TeacherFooter";
+export { default as TeacherFooterIcons } from "./TeacherFooterIcons";
+export { default as ToastModal } from "./ToastModal";

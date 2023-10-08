ae205d3e9962f132fb94f6d667f478698d2a529d
Seojisoo20191941 <amy0480@naver.com>
2023-10-08 23:20:51 +0900

    [ feat ] 출결 수정 로직 추가

--- src/components/common/AttendanceDoubleCheckingModal.tsx
+++ src/components/common/AttendanceDoubleCheckingModal.tsx
@@ -20,12 +20,15 @@ export default function AttendanceDoubleCheckingModal(props: AttendanceDoubleChe
   const [isImpossibleModalOpen, setIsImpossibleModalOpen] = useState(false);
 
   function handleBackToCheckAttendance() {
+    setAttendanceData({ ...attendanceData, status: "" });
+
     setIsCheckingModalOpen(false);
   }
 
   const { mutate: updateAttendance } = useMutation(updateScheduleAttendance, {
     onSuccess: () => {
       navigate("/complete-check-attendance", { state: ATTENDANCE_CHECK_RESPONSE });
+      setAttendanceData({ ...attendanceData, status: "" });
     },
     onError: () => {
       // if (err?.response?.data?.message) {
--- src/components/manageLesson/AttendanceInform.tsx
+++ src/components/manageLesson/AttendanceInform.tsx
@@ -70,7 +70,7 @@ export default function AttendanceInform(props: AttendanceInformProps) {
               )}
             </>
           ) : (
-            <NoCheckPageAttendanceButton />
+            <NoCheckPageAttendanceButton onClick={handleOpenCheckAttendanceModal} />
           )}
         </StatusWrapper>
       </AttnedanceInformBox>

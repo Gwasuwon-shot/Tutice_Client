1ddeea569a0462f3733df6b85b6f0a83c544e1b2
eunbeann <ds5bean23@gmail.com>
2023-10-08 22:58:57 +0900

    [ feat ] 임시 alert  추가

--- src/components/login/SocialLoginContent.tsx
+++ src/components/login/SocialLoginContent.tsx
@@ -3,10 +3,17 @@ import { RoundBottomButton } from "../common";
 import SocialLoginButton from "./SocialLoginButton";
 
 export default function SocialLoginContent() {
+
+  function yetAlert {
+    alert("해당 서비스는 아직 사용 불가능합니다. 이메일로 로그인 해주세요 ")
+  }
+
   return (
     <ButtonContainer>
-      <SocialLoginButton $bgcolor="#FFE500">카카오톡으로 시작하기</SocialLoginButton>
-      <SocialLoginButton $bgcolor="#03C75A">네이버로 시작하기</SocialLoginButton>
+      <SocialLoginButton onclick={yetAlert} $bgcolor="#FFE500">
+        카카오톡으로 시작하기
+      </SocialLoginButton>
+      <SocialLoginButton onclick={yetAlert} $bgcolor="#03C75A">네이버로 시작하기</SocialLoginButton>
     </ButtonContainer>
   );
 }

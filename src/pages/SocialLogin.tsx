a5a92360ce50919ec28b6f5b4100b73fbd041987
eunbeann <ds5bean23@gmail.com>
2023-10-08 22:13:35 +0900

    [ feat ] 소셜 로그인 합치기

--- src/pages/SocialLogin.tsx
+++ src/pages/SocialLogin.tsx
@@ -1,5 +1,47 @@
-import React from "react";
+import { styled } from "styled-components";
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
+
+import SocialLoginHeader from "../components/login/SocialLoginHeader";
+import SocialLoginContent from "../components/login/SocialLoginContent";
+import SocialLoginManaging from "../components/login/SocialLoginManaging";
+import { isCookieAuthenticated, isCookieNull, isLogin } from "../utils/common/isLogined";
 
 export default function SocialLogin() {
-  return <div></div>;
+  const navigate = useNavigate();
+
+  //f 로그인 되어 있는지 확인
+  const LoginChecked = isLogin() || isCookieNull() || isCookieAuthenticated();
+
+  useEffect(() => {
+    if (LoginChecked) {
+      navigate("/home");
+    }
+  }, [LoginChecked, navigate]);
+
+  return (
+    <>
+      {LoginChecked ? null : (
+        <>
+          <Container>
+            <SocialLoginHeader />
+          </Container>
+          <CenterWrapper>
+            <SocialLoginContent />
+            <SocialLoginManaging />
+          </CenterWrapper>
+        </>
+      )}
+    </>
+  );
 }
+
+const Container = styled.div`
+  margin-left: 1.4rem;
+`;
+
+const CenterWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+`;

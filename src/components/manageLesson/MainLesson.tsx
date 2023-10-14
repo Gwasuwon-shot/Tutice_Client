e5965737451a615df3bdb659554854748ae713ef
HAE2NI <hae0_02ni@ewhain.net>
2023-10-14 15:22:39 +0900

    [ feat ] manageLesson 디자인에 맞게 수정

--- src/components/manageLesson/MainLesson.tsx
+++ src/components/manageLesson/MainLesson.tsx
@@ -2,9 +2,8 @@ import { styled } from "styled-components";
 import { NextArrowManageLessonIc } from "../../assets";
 import { STUDENT_COLOR } from "../../core/common/studentColor";
 import useMoveToLessonDetail from "../../hooks/useMoveToLessonDetail";
-import StudentColorBox from "../common/StudentColorBox";
 import SubjectLabel from "../common/SubjectLabel";
-import TreeProgress from "../common/TreeProgress";
+import ManageStudentColorBox from "./ManageStudentColorBox";
 
 interface MainLessonProps {
   idx: number;
@@ -26,18 +25,17 @@ export default function MainLesson(props: MainLessonProps) {
     <MainLessonBox onClick={() => handleMoveToManageLessonDetail(idx)}>
       <MainLessonWrapperContainer>
         <MainLessonWrapper>
-          <StudentColorBox backgroundColor={STUDENT_COLOR[idx % 10]} />
+          <ManageStudentColorBox backgroundColor={STUDENT_COLOR[idx % 10]} />
           <StudentNameWrapper>{studentName}</StudentNameWrapper>
           <SubjectLabel subject={subject} backgroundColor={STUDENT_COLOR[idx % 10]} color="#5B6166" />
-          <DaysWrapper>
-            {dayOfWeekList.map((day, idx) => (
-              <>{checkIsLastDay(idx, day)}</>
-            ))}
-          </DaysWrapper>
         </MainLessonWrapper>
-        <TreeProgress progress={percent} width={23} />
+        <DaysWrapper>
+          <LessonInformation>진행예정</LessonInformation>
+          {dayOfWeekList.map((day, idx) => (
+            <>{checkIsLastDay(idx, day)}</>
+          ))}
+        </DaysWrapper>
       </MainLessonWrapperContainer>
-      <NextArrowManageLessonIcon />
     </MainLessonBox>
   );
 }
@@ -47,22 +45,24 @@ const MainLessonBox = styled.article`
   align-items: center;
 
   padding: 1rem;
-  margin-bottom: 1rem;
 
   border: 1px solid ${({ theme }) => theme.colors.grey70};
   border-radius: 0.8rem;
+
+  width: 14rem;
+  height: 14.4rem;
 `;
 
 const MainLessonWrapper = styled.div`
   display: flex;
   align-items: center;
-
-  margin-bottom: 1.2rem;
+  width: 11.6rem;
+  height: 1.6rem;
+  margin-bottom: 2.1rem;
+  gap: 0.7rem;
 `;
 
 const StudentNameWrapper = styled.h1`
-  margin: 0 0.6rem 0 1.5rem;
-
   color: ${({ theme }) => theme.colors.grey900};
   ${({ theme }) => theme.fonts.body01};
 `;
@@ -81,6 +81,7 @@ const MainLessonWrapperContainer = styled.section`
   width: 23rem;
 `;
 
-const NextArrowManageLessonIcon = styled(NextArrowManageLessonIc)`
-  margin-left: 2rem;
+const LessonInformation = styled.h2`
+  ${({ theme }) => theme.fonts.body04};
+  color: ${({ theme }) => theme.colors.grey500};
 `;

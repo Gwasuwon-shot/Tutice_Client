1949d6f14826df0cafe9911906b31a3c72645b3a
eunbeann <ds5bean23@gmail.com>
2023-10-15 17:24:09 +0900

    [ fix ] 버그픽스

--- src/components/login/SocialLoginContent.tsx
+++ src/components/login/SocialLoginContent.tsx
@@ -3,9 +3,8 @@ import { RoundBottomButton } from "../common";
 import SocialLoginButton from "./SocialLoginButton";
 
 export default function SocialLoginContent() {
-
-  function yetAlert {
-    alert("해당 서비스는 아직 사용 불가능합니다. 이메일로 로그인 해주세요 ")
+  function yetAlert() {
+    alert("해당 서비스는 아직 사용 불가능합니다. 이메일로 로그인 해주세요 ");
   }
 
   return (
@@ -13,7 +12,9 @@ export default function SocialLoginContent() {
       <SocialLoginButton onclick={yetAlert} $bgcolor="#FFE500">
         카카오톡으로 시작하기
       </SocialLoginButton>
-      <SocialLoginButton onclick={yetAlert} $bgcolor="#03C75A">네이버로 시작하기</SocialLoginButton>
+      <SocialLoginButton onclick={yetAlert} $bgcolor="#03C75A">
+        네이버로 시작하기
+      </SocialLoginButton>
     </ButtonContainer>
   );
 }

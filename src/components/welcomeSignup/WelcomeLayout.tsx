636aa5da2bf27dae441951abeb520ab789e951d7
pinktopaz <heather128@ewhain.net>
2023-10-18 03:36:37 +0900

    [ fix ] 알림-환영합니다 case에 맞게 수정

--- src/components/welcomeSignup/WelcomeLayout.tsx
+++ src/components/welcomeSignup/WelcomeLayout.tsx
@@ -3,22 +3,23 @@ import { useRecoilValue } from "recoil";
 import { styled } from "styled-components";
 import { userRoleData } from "../../atom/loginUser/loginUser";
 import { useNavigate } from "react-router-dom";
+import useGetAllLessons from "../../hooks/useGetAllLessons";
 
 export default function WelcomeLayout() {
   const navigate = useNavigate();
   const userRole = useRecoilValue(userRoleData);
+  const { lessonList } = useGetAllLessons();
 
   useEffect(() => {
-    if (userRole !== "선생님") {
-      checkAlarmAlertShow();
-    }
+    checkAlarmAlertShow();
   }, []);
 
   async function checkAlarmAlertShow() {
     const permission = await Notification.requestPermission();
 
     if (permission == "granted" || permission == "denied") {
-      if (userRole !== "선생님") {
+      if (userRole == "선생님") {
+        lessonList.length ? navigate("/home") : navigate("/tree");
       } else navigate("/home");
     } else {
       navigate("/alert");
@@ -28,9 +29,7 @@ export default function WelcomeLayout() {
 
   return (
     <>
-      <Container>
-        {/* {!isWelcome ? <AfterSignup setIsWelcome={setIsWelcome} /> : <AlertSignup setIsWelcome={setIsWelcome} />} */}
-      </Container>
+      <Container></Container>
     </>
   );
 }

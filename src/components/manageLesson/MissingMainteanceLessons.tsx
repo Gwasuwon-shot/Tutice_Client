0eb88f3e72556d336f7393e2a230c299073639df
HAE2NI <hae0_02ni@ewhain.net>
2023-10-17 21:45:02 +0900

    [ feat ] 연장 놓친 수업들 추가

---
+++ src/components/manageLesson/MissingMainteanceLessons.tsx
@@ -0,0 +1,44 @@
+import { styled } from "styled-components";
+import useGetMissingMaintenanceLesson from "../../hooks/useGetMissingMaintenanceLesson";
+import MissingMainteanceLesson from "./MissingMainteanceLesson";
+
+interface MissingLessonProp {
+  lesson: {
+    idx: number;
+    studentName: string;
+    subject: string;
+    count: number;
+  };
+}
+
+interface MissingMainteanceLessonsProps {
+  isClickedEdit: boolean;
+}
+
+export default function MissingMainteanceLessons(props: MissingMainteanceLessonsProps) {
+  const { isClickedEdit } = props;
+  const { missingMaintenanceLessonList } = useGetMissingMaintenanceLesson();
+
+  return (
+    <>
+      <MissingMainteanceLessonWrapper>
+        {missingMaintenanceLessonList &&
+          missingMaintenanceLessonList?.map(({ lesson }: MissingLessonProp) => (
+            <MissingMainteanceLesson
+              isClickedEdit={isClickedEdit}
+              key={lesson.idx}
+              idx={lesson.idx}
+              studentName={lesson.studentName}
+              subject={lesson.subject}
+            />
+          ))}
+      </MissingMainteanceLessonWrapper>
+    </>
+  );
+}
+
+const MissingMainteanceLessonWrapper = styled.article`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 1.1rem;
+`;

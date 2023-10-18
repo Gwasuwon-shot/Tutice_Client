c4caf499dfe4a50a6a3dd2d3629b9beb26fac508
eunbeann <ds5bean23@gmail.com>
2023-10-19 00:44:27 +0900

    [ etc ] 무의미 removeCokkie 삭제

--- src/api/patchLogout.ts
+++ src/api/patchLogout.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { getCookie, removeCookie } from "./cookie";
+import { getCookie } from "./cookie";
 
 export async function patchLogout() {
   const data = await axios.patch(`${import.meta.env.VITE_APP_BASE_URL}/api/user/logout`, null, {
@@ -7,6 +7,5 @@ export async function patchLogout() {
       Authorization: `Bearer ${getCookie("accessToken")}`,
     },
   });
-  removeCookie("accessToken", { path: "/" });
   return data;
 }

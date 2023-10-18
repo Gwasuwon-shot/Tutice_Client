ff4335f581e3347ada14e55ad684e9d9bdf61620
eunbeann <ds5bean23@gmail.com>
2023-10-18 20:22:18 +0900

    [ feat ] 로그아웃 api 추가

---
+++ src/api/patchLogout.ts
@@ -0,0 +1,12 @@
+import axios from "axios";
+import { getCookie } from "./cookie";
+
+export async function patchLogout() {
+  const data = await axios.patch(`${import.meta.env.VITE_APP_BASE_URL}/api/user/logout`, null, {
+    headers: {
+      // Authorization: `Bearer ${getCookie("accessToken")}`,
+      Authorization: `${getCookie("accessToken")}`,
+    },
+  });
+  return data;
+}

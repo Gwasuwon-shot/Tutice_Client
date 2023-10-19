b937db594442683293a0c775b88d4432dae019e7
HAE2NI <hae0_02ni@ewhain.net>
2023-10-19 22:35:35 +0900

    [ etc ] deleteLesson API

--- src/api/deleteLesson.ts
+++ src/api/deleteLesson.ts
@@ -2,12 +2,16 @@ import axios from "axios";
 import { getCookie } from "./cookie";
 
 export async function deleteLesson(lessonIdx: number) {
-  const data = await axios.delete(`${import.meta.env.VITE_APP_BASE_URL}/api/lesson/${lessonIdx}`, {
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: `Bearer ${getCookie("accessToken")}`,
-    },
-  });
+  const data = await axios.delete(
+    `${import.meta.env.VITE_APP_BASE_URL}/api/lesson/${lessonIdx}`,
 
-  return data.data.data;
+    {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${getCookie("accessToken")}`,
+      },
+    },
+  );
+
+  return data;
 }

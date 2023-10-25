f2a29dd113e050e5c8807351e22a99a88ee4e9fd
HAE2NI <hae0_02ni@ewhain.net>
2023-10-25 15:54:03 +0900

    [ etc ] lessonListType 수정

--- src/type/manageLesson/lessonListType.ts
+++ src/type/manageLesson/lessonListType.ts
@@ -1,8 +1,14 @@
+export interface latestRegularScheduleType {
+  dayOfWeek: string;
+  startTime: string;
+  endTime: string;
+}
+
 export interface lessonListType {
   idx: number;
   studentName: string;
   subject: string;
   percent: number;
-  dayOfWeekList: string[];
-  isClickedEdit: boolean;
+  isFinished: boolean;
+  latestRegularSchedule: latestRegularScheduleType;
 }

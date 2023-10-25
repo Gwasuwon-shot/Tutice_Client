5e56527f1d17e44b00efd4fac17bea81a2e97610
HAE2NI <hae0_02ni@ewhain.net>
2023-10-25 21:19:36 +0900

    [ design ] NoCheckLesson 있을 때 검사 코드 추가

--- src/pages/NoAttendanceCheck.tsx
+++ src/pages/NoAttendanceCheck.tsx
@@ -1,11 +1,14 @@
 import NoAttendanceheader from "../components/noAttendance/NoAttendanceheader";
 import NoCheckLesson from "../components/noAttendance/NoCheckLesson";
+import useGetMissingAttendanceSchedule from "../hooks/useGetMissingAttendanceSchedule";
 
 export default function NoAttendanceCheck() {
+  const { missingAttendanceSchedule } = useGetMissingAttendanceSchedule();
+
   return (
     <>
       <NoAttendanceheader />
-      <NoCheckLesson />
+      {missingAttendanceSchedule && <NoCheckLesson />}
     </>
   );
 }

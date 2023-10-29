18583c22439d279ddb54030718a644f124e23ebd
Seojisoo20191941 <amy0480@naver.com>
2023-10-29 18:18:04 +0900

    [ fix ] 비동기 처리

---
+++ src/components/RegularLesson/ConnectParentsAndTeacher.tsx
@@ -0,0 +1,41 @@
+import { useEffect } from "react";
+import { Cookies } from "react-cookie";
+import { useNavigate, useParams } from "react-router-dom";
+import { useRecoilState } from "recoil";
+import { patchLessonParents } from "../../api/patchLessonParents";
+import { connectLessonId } from "../../atom/registerLesson/registerLesson";
+import { blockAccess } from "../../utils/common/privateRoute";
+
+export default function ConnectParentsAndTeacher() {
+  const { lessonId } = useParams();
+  const [lessonIndex, setLessonIndex] = useRecoilState(connectLessonId);
+
+  const navigate = useNavigate();
+
+  const cookies = new Cookies();
+
+  const cookie = (name: string) => {
+    return cookies.get(name);
+  };
+
+  async function connectParentsAndTeacher() {
+    if (lessonId) {
+      const data = await patchLessonParents(lessonIndex).then(() => navigate("/home"));
+      return data;
+    }
+  }
+
+  useEffect(() => {
+    if (lessonId) {
+      setLessonIndex(lessonId);
+    }
+
+    if (!blockAccess()) {
+      connectParentsAndTeacher();
+    } else {
+      navigate("/");
+    }
+  }, []);
+
+  return <div></div>;
+}
